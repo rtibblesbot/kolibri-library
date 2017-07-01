eea00a168331226fcd554557a2ca89233e34e7ba
David Hu <davidhu91@gmail.com>
2017-06-30 17:07:41 -0700

    Get scraping touchableearth.org to work!
    This is a first pass. Scrapes all content from touchableearth.org, which
    includes videos and images. Two topic tiers: country and category.
    
    Test and preview on:
    - content workshop/studio/curator: https://contentworkshop.learningequality.org/channels/66cef05505fa550b970e69c3623e82ba
    - kolibri: http://35.186.190.71/learn/#/topics/66cef05505fa550b970e69c3623e82ba

--- touchableearth/te_chef.py
+++ touchableearth/te_chef.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python
 
-from le_utils.constants import content_kinds, file_formats, licenses, languages
-from ricecooker.chefs import SushiChef
-from ricecooker.classes import nodes, files
+"""
+Sushi Chef for Touchable Earth: http://www.touchableearth.org/
+Consists of videos and images.
+"""
 
-from chefdata.data import SOURCE_DOMAIN, SOURCE_ID, CHANNEL_TITLE, CHANNEL_THUMBNAIL, DATA_SOURCE
+import os
+import requests
+import tempfile
+import urllib
+from urllib.parse import urlparse, parse_qs
+
+from bs4 import BeautifulSoup
+
+from le_utils.constants import content_kinds, file_formats, languages
+from ricecooker.chefs import SushiChef
+from ricecooker.classes import nodes, files, licenses
+from ricecooker.utils.caching import CacheForeverHeuristic, FileCache, CacheControlAdapter, InvalidatingCacheControlAdapter
+from ricecooker.utils.browser import preview_in_browser
+from ricecooker.utils.html import download_file
+from ricecooker.utils.zip import create_predictable_zip
+
+
+sess = requests.Session()
+cache = FileCache('.webcache')
+forever_adapter= CacheControlAdapter(heuristic=CacheForeverHeuristic(), cache=cache)
+
+sess.mount('http://www.touchableearth.org', forever_adapter)
+
+TE_LICENSE = licenses.SpecialPermissionsLicense(
+    description="Permission has been granted by Touchable Earth to"
+    " distribute this content through Kolibri."
+)
 
 
 class TouchableEarthChef(SushiChef):
@@ -14,10 +41,10 @@ class TouchableEarthChef(SushiChef):
     We'll call its `main()` method from the command line script.
     """
     channel_info = {
-        'CHANNEL_SOURCE_DOMAIN': SOURCE_DOMAIN,
-        'CHANNEL_SOURCE_ID': SOURCE_ID,
-        'CHANNEL_TITLE': CHANNEL_TITLE,
-        'CHANNEL_THUMBNAIL': CHANNEL_THUMBNAIL,
+        'CHANNEL_SOURCE_DOMAIN': "www.touchableearth.org",
+        'CHANNEL_SOURCE_ID': "touchable-earth",
+        'CHANNEL_TITLE': "Touchable Earth",
+        'CHANNEL_THUMBNAIL': "https://d1iiooxwdowqwr.cloudfront.net/pub/appsubmissions/20140218003206_PROFILEPHOTO.jpg",
     }
 
     def construct_channel(self, **kwargs):
@@ -35,49 +62,223 @@ class TouchableEarthChef(SushiChef):
         )
 
         # build tree
-        _build_tree(channel, DATA_SOURCE)
+        add_countries_to_channel(channel)
 
         return channel
 
 
+def add_countries_to_channel(channel):
+    doc = get_parsed_html_from_url("http://www.touchableearth.org/places/")
+    places = doc.select("div.places-row a.custom-link")
 
-def _build_tree(parent, data_source):
+    for place in places:
+        title = place.text.strip()
+        href = place["href"]
+        channel.add_child(scrape_country(title, href))
+
+
+def scrape_country(title, country_url):
     """
-    Parse nodes given in `sourcetree` and add as children of `node`.
+    title: China
+    country_url: http://www.touchableearth.org/china-facts-welcome/
     """
-    for child_source_node in data_source:
+    print("Scraping country node: %s (%s)" % (title, country_url))
 
-        if child_source_node.get('children'):
-            child_node = nodes.TopicNode(
-                source_id=child_source_node["id"],
-                title=child_source_node["title"],
-            )
-            parent.add_child(child_node)
-            _build_tree(child_node, child_source_node.get("children", []))
+    doc = get_parsed_html_from_url(country_url)
+    country = doc.select_one(".breadcrumbs .taxonomy.category")
+    href = country["href"]
+    title = country.text.strip()
 
+    topic = nodes.TopicNode(source_id=href, title=title)
+    add_topics_to_country(topic, href)
+
+    return topic
+
+
+def add_topics_to_country(country_node, country_url):
+    """
+    country_url: http://www.touchableearth.org/china/
+    """
+    doc = get_parsed_html_from_url(country_url)
+    categories = doc.select(".sub_cat_listing a")
+
+    for category in categories:
+        url = category["href"]
+        title = category.text.strip()
+        country_node.add_child(scrape_category(title, url))
+
+
+def scrape_category(title, category_url):
+    """
+    title: Culture
+    category_url: http://www.touchableearth.org/china/culture/
+        ... redirects to: http://www.touchableearth.org/china-culture-boys-clothing/
+    """
+    print("  Scraping category node: %s (%s)" % (title, category_url))
+
+    category_node = nodes.TopicNode(source_id=category_url, title=title)
+
+    # Iterate over each item in the "subway" sidebar menu on the left.
+    doc = get_parsed_html_from_url(category_url)
+    content_items = doc.select(".post_title_sub .current_post")
+    slugs_added = set()
+
+    for content in content_items:
+        slug = content.select_one(".get_post_title")["value"]
+
+        # Skip duplicates ... seems like the Touchable Earth website has them!
+        if slug in slugs_added:
+            continue
         else:
-            child_node = nodes.VideoNode(
-                source_id=child_source_node["id"],
-                title=child_source_node["title"],
-                license=child_source_node["license"],
-                description=create_description(child_source_node),
-                derive_thumbnail=True,
-                files=[files.YouTubeVideoFile(youtube_id=child_source_node['youtube_id'])],
-            )
-            for language in child_source_node['subtitle_langs']:
-                child_node.add_file(files.YouTubeSubtitleFile(youtube_id=child_source_node['youtube_id'], language=language))
-            parent.add_child(child_node)
+            slugs_added.add(slug)
 
-    return parent
+        title = content.select_one(".get_post_title2")["value"]
+        site_url = content.select_one(".site_url")["value"]
+        url = "%s/%s" % (site_url, slug)
+
+        content_node = scrape_content(title, url)
+        if content_node:
+            category_node.add_child(content_node)
+
+    return category_node
+
+
+def scrape_content(title, content_url):
+    """
+    title: Boys' clothing
+    content_url: http://www.touchableearth.org/china-culture-boys-clothing/
+    """
+    print("    Scraping content node: %s (%s)" % (title, content_url))
+
+    doc = get_parsed_html_from_url(content_url)
+    if not doc:  # 404
+        return None
+
+    description = create_description(doc.select_one(".tab-content"))
+    base_node_attributes = {
+        "source_id": content_url,
+        "title": title,
+        "license": TE_LICENSE,
+        "description": description,
+    }
+
+    youtube_iframe = doc.select_one(".video-container iframe")
+    if youtube_iframe:
+        youtube_url = doc.select_one(".video-container iframe")["src"]
+        youtube_id = get_youtube_id_from_url(youtube_url)
+
+        video_node = nodes.VideoNode(
+            **base_node_attributes,
+            derive_thumbnail=True,
+            files=[files.YouTubeVideoFile(youtube_id=youtube_id)],
+        )
+
+        # TODO(davidhu): Get subtitles in other languages
+
+        return video_node
+
+    img = doc.select_one(".uncode-single-media-wrapper img")
+    if img:
+        img_src = img["data-guid"] or img["src"]
+        destination = tempfile.mkdtemp()
+        download_file(img_src, destination, request_fn=make_request, filename="image.jpg")
+
+        with open(os.path.join(destination, "index.html"), "w") as f:
+            f.write("""
+                <html>
+                <head></head>
+                <body>
+                    <img src="image.jpg" style="width: 100%;" />
+                </body>
+            """)
+
+        zip_path = create_predictable_zip(destination)
+
+        return nodes.HTML5AppNode(
+            **base_node_attributes,
+            files=[files.HTMLZipFile(zip_path)],
+        )
+
+    return None
+
 
 def create_description(source_node):
-    description = source_node.get("about") or ""
-    if source_node.get("transcript"):
-        description += "\n\nTRANSCRIPT: " + source_node.get('transcript')
-    if source_node.get('info'):
-        description += "\n\nMORE INFO: " + source_node.get('info')
+    panes = source_node.select(".tab-pane")
+    about = panes[0].text.strip()
+    transcript  = panes[1].text.strip()
+
+    description = about
+    if transcript:
+        description += "\n\nTRANSCRIPT: %s" % transcript
+
     return description
 
+
+# From https://stackoverflow.com/a/7936523
+def get_youtube_id_from_url(value):
+    """
+    Examples:
+    - http://youtu.be/SA2iWivDJiE
+    - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
+    - http://www.youtube.com/embed/SA2iWivDJiE
+    - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
+    """
+    query = urlparse(value)
+    if query.hostname == 'youtu.be':
+        return query.path[1:]
+    if query.hostname in ('www.youtube.com', 'youtube.com'):
+        if query.path == '/watch':
+            p = parse_qs(query.query)
+            return p['v'][0]
+        if query.path[:7] == '/embed/':
+            return query.path.split('/')[2]
+        if query.path[:3] == '/v/':
+            return query.path.split('/')[2]
+    # fail?
+    return None
+
+
+# This is taken and modified from https://github.com/fle-internal/sushi-chef-ck12/blob/cb0d538b6857f399271d0895967727f635e58ee0/chef.py#L85
+# TODO(david): Extract to a util library
+def make_request(url, clear_cookies=True, timeout=60, *args, **kwargs):
+    if clear_cookies:
+        sess.cookies.clear()
+
+    # resolve ".." and "." references in url path to ensure cloudfront doesn't barf
+    purl = urlparse(url)
+    newpath = urllib.parse.urljoin(purl.path + "/", ".").rstrip("/")
+    url = purl._replace(path=newpath).geturl()
+
+    retry_count = 0
+    max_retries = 5
+    while True:
+        try:
+            response = sess.get(url, timeout=timeout, *args, **kwargs)
+            break
+        except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as e:
+            retry_count += 1
+            print("Error with connection ('{msg}'); about to perform retry {count} of {trymax}."
+                  .format(msg=str(e), count=retry_count, trymax=max_retries))
+            time.sleep(retry_count * 1)
+            if retry_count >= max_retries:
+                return Dummy404ResponseObject(url=url)
+
+    if response.status_code != 200:
+        print("NOT FOUND:", url)
+        return None
+    elif not response.from_cache:
+        print("NOT CACHED:", url)
+
+    return response
+
+
+def get_parsed_html_from_url(url, *args, **kwargs):
+    request = make_request(url, *args, **kwargs)
+    if not request:
+        return None
+
+    html = request.content
+    return BeautifulSoup(html, "html.parser")
 
 
 if __name__ == '__main__':
