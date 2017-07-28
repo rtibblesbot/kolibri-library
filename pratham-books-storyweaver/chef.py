bc9ba813eb1d8df735d3a72d028d519b9013126b
Lingyi Wang <lywang07@gmail.com>
2017-07-28 16:20:32 -0700

    change content_id of AS related stories

--- pratham-books-storyweaver/chef.py
+++ pratham-books-storyweaver/chef.py
@@ -3,6 +3,7 @@
 import requests
 import argparse
 import cairosvg
+import uuid
 from bs4 import BeautifulSoup
 from itertools import groupby
 
@@ -13,6 +14,7 @@ from le_utils.constants import licenses
 from ricecooker.classes.licenses import get_license
 from ricecooker.utils.caching import CacheForeverHeuristic, FileCache, CacheControlAdapter
 from ricecooker.config import LOGGER, DOWNLOAD_SESSION
+from ricecooker.utils.html import WebDriver
 
 # Variables
 BASE_URL = 'https://storyweaver.org.in'
@@ -28,7 +30,7 @@ TEMPLATE_LEVEL_TITLE = 'Level {num}'
 # Cache
 session = requests.Session()
 cache = FileCache('.webcache')
-basic_adapter = CacheControlAdapter(cache=cache)
+basic_adapter = CacheControlAdapter()
 forever_adapter = CacheControlAdapter(heuristic=CacheForeverHeuristic(), cache=cache)
 
 session.mount('https://storyweaver.org.in', basic_adapter)
@@ -54,6 +56,42 @@ def edit_img_ext(url):
     else:
         result = url
     return  result
+
+
+"""
+Get a list of all books in African Storybooks website.
+"""
+def get_AS_booklist_dict():
+    with WebDriver("http://www.africanstorybook.org/", delay=20000) as driver:
+
+        js_code = """
+            var textElement = document.createElement("textarea");
+            function decodeHtml(html) {
+                textElement.innerHTML = html;
+                return textElement.value;
+            }
+
+            // Safely decode HTML entities in title field.
+            for (var i = bookItems.length - 1; i >= 0; i--) {
+                bookItems[i].title = decodeHtml(bookItems[i].title);
+            }
+
+            return bookItems;
+        """
+
+        books = driver.execute_script(js_code)
+        names = []
+        # Create a list of book names
+        for book in books:
+            names.append(book['title'].lower())
+        dictionary = {}
+        # Create a dictionary of books with name as key
+        for (name, book) in zip(names, books):
+            if name in dictionary:
+                dictionary[name].append(book)
+            else:
+                dictionary[name] = [book]
+        return dictionary
 
 
 """
@@ -84,7 +122,7 @@ def get_all_publishers():
 Get all the information about the books from the json file of each page 
 for each publisher.
 """
-def get_books_from_results(search_results, list):
+def get_books_from_results(search_results, list, pub):
     # Loop through all the books for one page
     for i in range(len(search_results)):
         download_link = BASE_URL + search_results[i]['links'][0]['download']['low_res_pdf']
@@ -96,7 +134,8 @@ def get_books_from_results(search_results, list):
             'description': search_results[i]['synopsis'],
             'thumbnail': edit_img_ext(search_results[i]['image_url']),
             'language': search_results[i]['language'],
-            'level': search_results[i]['reading_level']
+            'level': search_results[i]['reading_level'],
+            'publisher': pub,
         }
         list.append(book_dict)
     return list
@@ -126,7 +165,7 @@ def books_for_each_publisher(pub):
     LOGGER.info('\tThere is(are) in total %s page(s) for %s......\n' % (str(total_pages), pub))
 
     # List of books for the first page
-    list = get_books_from_results(data['search_results'], list)
+    list = get_books_from_results(data['search_results'], list, pub)
 
     # get the rest of the pages' books
     for i in range(1, total_pages):
@@ -136,7 +175,7 @@ def books_for_each_publisher(pub):
             page_url = TEMPLATE_URL.format(page_num=i+1, publisher_name=name)
         response = session.get(page_url, headers=_headers)
         data = response.json() 
-        list = get_books_from_results(data['search_results'], list)
+        list = get_books_from_results(data['search_results'], list, pub)
 
     LOGGER.info('\tFinished getting all the books for %s\n\t====================\n' % (pub))
     return list
@@ -178,13 +217,16 @@ def add_topic_pub(channel):
         )
 
         channel.add_child(pubtopic)
-        add_sub_topic_lang(publisher, pubtopic)
+        as_booklist = {}
+        if pub_title == 'African Storybook Initiative':
+            as_booklist = get_AS_booklist_dict()
+        add_sub_topic_lang(publisher, pubtopic, as_booklist)
 
 
 """
 Add subtopics about languages under a specific publisher topic.
 """
-def add_sub_topic_lang(publisher, pubtopic):
+def add_sub_topic_lang(publisher, pubtopic, as_booklist):
     # Get all the books info for each publisher
     book_list = books_for_each_publisher(publisher)
     sorted_language_list = group_books(book_list, 'language')
@@ -198,13 +240,13 @@ def add_sub_topic_lang(publisher, pubtopic):
             title = lang,
         )
         pubtopic.add_child(langsubtopic)
-        add_sub_topic_level(publisher, lang, langsubtopic, sorted_language_list)
+        add_sub_topic_level(publisher, lang, langsubtopic, sorted_language_list, as_booklist)
 
 
 """
 Add subtopics about levels of reading under a specific language subtopic.
 """
-def add_sub_topic_level(publisher, lang, langsubtopic, list):
+def add_sub_topic_level(publisher, lang, langsubtopic, list, as_booklist):
     sorted_level_list = group_books(list[lang], 'level')
     levels = sorted_level_list.keys()
 
@@ -216,24 +258,50 @@ def add_sub_topic_level(publisher, lang, langsubtopic, list):
             title = TEMPLATE_LEVEL_TITLE.format(num=level),
         )
         langsubtopic.add_child(levelsubtopic)
-        add_node_document(level, levelsubtopic, sorted_level_list)
+        add_node_document(level, levelsubtopic, sorted_level_list, as_booklist)
+
+
+"""
+Check if the story in StoryWeaver is also in African Storybooks.
+"""
+def check_if_story_in_AS(as_booklist, story_name):
+    result = as_booklist.get(story_name.lower().rstrip())
+    if result != None and len(result) == 1:
+        return True, result[0]['id']
+    else:
+        return False, None 
 
 
 """
 Add books under a specific level of reading.
 """
-def add_node_document(level, levelsubtopic, list):
+def add_node_document(level, levelsubtopic, list, as_booklist):
     # Add books according to level, language and publisher
     for item in list[level]:
+        # initailize the source domain and content_id
+        domain = uuid.uuid5(uuid.NAMESPACE_DNS, 'storyweaver.org.in')
+        book_id = str(item['source_id'])
+
+        """ 
+        If the publisher is AS and the book is found, 
+        then change the source_domain and content_id
+        """
+        if item['publisher'] == 'African Storybook Initiative':
+            check = check_if_story_in_AS(as_booklist, item['title'])
+            if check[0] == True:
+                domain = uuid.uuid5(uuid.NAMESPACE_DNS,'www.africanstorybook.org')
+                book_id = check[1]
+
         document_file = DocumentFile(path=item['link'])
         book = DocumentNode(
-            title=item['title'], 
-            source_id=str(item['source_id']), 
+            title = item['title'], 
+            source_id = book_id, 
             author = item['author'],
-            files=[document_file], 
-            license=get_license(licenses.CC_BY),
+            files = [document_file], 
+            license = get_license(licenses.CC_BY),
             thumbnail = item.get('thumbnail'),
             description = item['description'],
+            domain_ns = domain,
         )
         levelsubtopic.add_child(book)
 
