d406d77b27ec9f99cc772ca60908d5c8fe74f1a8
David Hu <davidhu91@gmail.com>
2017-08-22 18:12:57 -0700

    Update thumbnail again

--- open-osmosis/chef.py
+++ open-osmosis/chef.py
@@ -52,7 +52,7 @@ class OpenOsmosisChef(SushiChef):
         'CHANNEL_SOURCE_DOMAIN': "open.osmosis.org",
         'CHANNEL_SOURCE_ID': "open-osmosis",
         'CHANNEL_TITLE': "Open Osmosis",
-        'CHANNEL_THUMBNAIL': "https://yt3.ggpht.com/-Hte1SseOlEM/AAAAAAAAAAI/AAAAAAAAAAA/vli0lmZ-5Iw/s900-c-k-no-mo-rj-c0xffffff/photo.jpg",
+        'CHANNEL_THUMBNAIL': "https://d3cdo0emj8d2qc.cloudfront.net/assets/f78c3f1d2be258bd84c85cb1f342e9f7343c798b.png",
         'CHANNEL_DESCRIPTION': "A study tool built for tomorrow's doctors and health workers.",
     }
 
