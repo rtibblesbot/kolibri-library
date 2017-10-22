00795560110445cce97a9c212b0ccb434c5bd8f3
David Hu <davidhu91@gmail.com>
2017-10-22 08:13:21 -0700

    First pass at assessments

--- open-osmosis/chef.py
+++ open-osmosis/chef.py
@@ -21,10 +21,10 @@ import youtube_dl
 
 from le_utils.constants import content_kinds, file_formats, languages
 from ricecooker.chefs import SushiChef
-from ricecooker.classes import nodes, files, licenses
+from ricecooker.classes import nodes, files, licenses, questions
 from ricecooker.utils.caching import CacheForeverHeuristic, FileCache, CacheControlAdapter, InvalidatingCacheControlAdapter
 from ricecooker.utils.browser import preview_in_browser
-from ricecooker.utils.html import download_file, WebDriver, minimize_html_css_js
+from ricecooker.utils.html import download_file, WebDriver
 from ricecooker.utils.zip import create_predictable_zip
 
 
@@ -32,15 +32,20 @@ sess = requests.Session()
 cache = FileCache('.webcache')
 forever_adapter = CacheControlAdapter(heuristic=CacheForeverHeuristic(), cache=cache)
 
-#sess.mount('http://', forever_adapter)
-#sess.mount('https://', forever_adapter)
+# XXX how to cache cloudfront?
+sess.mount('http://', forever_adapter)
+sess.mount('https://', forever_adapter)
 
 ydl = youtube_dl.YoutubeDL({
-    'quiet': True,
+    #'quiet': True,
     'no_warnings': True,
     'writesubtitles': True,
     'allsubtitles': True,
+    'ignoreerrors': True,  # Skip over deleted videos in a playlist
 })
+
+LICENSE = licenses.CC_BYLicense(
+    copyright_holder='Open Osmosis (open.osmosis.org)')
 
 
 class OpenOsmosisChef(SushiChef):
@@ -51,7 +56,7 @@ class OpenOsmosisChef(SushiChef):
     """
     channel_info = {
         'CHANNEL_SOURCE_DOMAIN': "open.osmosis.org",
-        'CHANNEL_SOURCE_ID': "open-osmosis",
+        'CHANNEL_SOURCE_ID': "open-osmosis-assessments",
         'CHANNEL_TITLE': "Open Osmosis",
         'CHANNEL_THUMBNAIL': "https://d3cdo0emj8d2qc.cloudfront.net/assets/f78c3f1d2be258bd84c85cb1f342e9f7343c798b.png",
         'CHANNEL_DESCRIPTION': "A study tool built for tomorrow's doctors and health workers.",
@@ -72,6 +77,9 @@ class OpenOsmosisChef(SushiChef):
             language = "en",
         )
 
+        fetch_assessments(channel)
+        return channel
+
         youtube_channel_url = 'https://www.youtube.com/channel/UCNI0qOojpkhsUtaQ4_2NUhQ/playlists'
 
         print("Fetching YouTube channel and videos metadata --"
@@ -87,9 +95,101 @@ class OpenOsmosisChef(SushiChef):
                     language="en")
             channel.add_child(playlist_topic)
             for video in playlist['entries']:
-                playlist_topic.add_child(fetch_video(video))
+                if video:
+                    playlist_topic.add_child(fetch_video(video))
 
         return channel
+
+
+def fetch_assessments(channel):
+    with WebDriver("https://open.osmosis.org/topics", delay=1000) as driver:
+        page_html = get_generated_html_from_driver(driver)
+        doc = BeautifulSoup(page_html, "html.parser")
+
+        for i, topic in enumerate(doc.select('.container .topic')):
+            link = topic.select_one('a')
+            href = link['href']
+            topic_id = href.split('/')[-1]
+            url = make_fully_qualified_url(href)
+            text = link.text.strip()
+            img = link.select_one('img')['src']
+
+            topic_node = nodes.TopicNode(source_id=topic_id,
+                    title=text, thumbnail=img)
+            fetch_assessment_item(driver, url, topic_node)
+            channel.add_child(topic_node)
+
+            # XXX
+            if i > 1:
+                break
+
+
+def _process_text_into_markdown(container_node):
+    markdown_text = ''
+
+    for node in container_node.children:
+        image = node.select_one('.models-media-Image')
+        if image:
+            src = image.select_one('img')['src']
+            credit = image.text.strip()
+            markdown_text += '![%s](%s)\n%s\n\n' % (credit, src, credit)
+        else:
+            text = node.get_text(separator="\n").strip()
+            if 'fwb' in node.get('class', []):
+                text = '**' + text + '**'
+            markdown_text += text + "\n\n"
+
+    return markdown_text
+
+
+QUESTIONS_PER_EXERCISE = 5
+
+def fetch_assessment_item(driver, topic_url, topic_node,
+        exercise_node=None, item_index=0):
+    driver.get(topic_url)
+    time.sleep(1)
+    item_url = driver.current_url
+    item_id = driver.current_url.split('/')[-1]
+
+    page_html = get_generated_html_from_driver(driver)
+    doc = BeautifulSoup(page_html, "html.parser")
+
+    question_markdown = _process_text_into_markdown(doc.select_one('#Content .stem'))
+    answers = [ans.text.strip() for ans in doc.select('.answers .ans div')]
+    correct = doc.select_one('.answers-explained .fwb').text.strip()
+
+    # TODO(david): Get videos from hints, e.g. in https://open.osmosis.org/item/142641
+    # TODO(david): Fine-tune line spacing for hints
+    hint_1 = _process_text_into_markdown(doc.select_one('.answers-explained .explain-ans'))
+    hint_2 = _process_text_into_markdown(doc.select_one('.answers-explained .explain'))
+    combined_hint = "%s\n\nMain Explanation\n---\n\n%s" % (hint_1, hint_2)
+
+    question = questions.SingleSelectQuestion(id=item_id, hints=combined_hint,
+            question=question_markdown, all_answers=answers,
+            correct_answer=correct)
+
+    if item_index % QUESTIONS_PER_EXERCISE == 0:
+        total_questions = int(doc.select_one('#Content .ques-count').text.split()[-1])
+        last_item = min(item_index + QUESTIONS_PER_EXERCISE, total_questions)
+        exercise_title = "%s %s-%s" % (topic_node.title, item_index + 1, last_item)
+        exercise_node = nodes.ExerciseNode(source_id=item_id,
+                title=exercise_title, license=LICENSE,
+                exercise_data={'randomize': False})
+        topic_node.add_child(exercise_node)
+
+    exercise_node.add_question(question)
+
+    # XXX
+    if item_index > 7:
+        return
+
+    next_link = doc.select_one('.ques-nav-right a')
+    if next_link:
+        next_href = doc.select_one('.ques-nav-right a')['href']
+        if not next_href.endswith('null'):
+            next_url = make_fully_qualified_url(next_href)
+            fetch_assessment_item(driver, next_url, topic_node, exercise_node,
+                    item_index + 1)
 
 
 # TODO(davidhu): Remove this when
@@ -198,6 +298,20 @@ def truncate_metadata(data_string):
     if len(data_string) > MAX_CHARS:
         data_string = data_string[:190] + " ..."
     return data_string
+
+
+def make_fully_qualified_url(url):
+    if url.startswith("//"):
+        return "http:" + url
+    if url.startswith("/"):
+        return "https://open.osmosis.org" + url
+    if not url.startswith("http"):
+        return "https://open.osmosis.org/" + url
+    return url
+
+
+def get_generated_html_from_driver(driver, tagname="html"):
+    return driver.execute_script("return document.getElementsByTagName('{tagname}')[0].innerHTML".format(tagname=tagname))
 
 
 if __name__ == '__main__':
