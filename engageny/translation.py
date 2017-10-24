447c397f5127dd9d6282e64d87e551048227c6a1
César López-Natarén <cesar@nataren.io>
2017-10-23 17:00:44 -0700

    Init the translation layer

--- engageny/translation.py
+++ engageny/translation.py
@@ -1,14 +1,23 @@
 from google.cloud import translate
 
 class Client:
-    def __init__(source_language='en', target_language=None, format_='text', model='nmt'):
+    def __init__(self, source_language='en', target_language=None, format_='text', model='nmt'):
         self.source_language = source_language
         self.target_language = target_language
         self.format = format_
         self.model = model
         self.client = translate.Client(target_language=self.target_language)
 
-    def translate(values):
+    def translate(self, values):
+        strings_to_translate = values if isinstance(values, list) else [values]
+        if self.source_language == 'en' and self.source_language == self.target_language:
+            return [
+                dict(
+                    detectedSourceLanguage=self.source_language,
+                    model='nop',
+                    translatedText=v
+                ) for v in strings_to_translate
+            ]
         return self.client.translate(
             values,
             target_language=self.target_language,
