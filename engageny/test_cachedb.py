a57252ac91cf4159a3d0348a2c66a5851a21f24d
César López-Natarén <cesar@nataren.io>
2017-11-11 15:34:34 -0800

    Test the basic operations of the cache

--- engageny/test_cachedb.py
+++ engageny/test_cachedb.py
@@ -13,11 +13,33 @@ class TestCacheDbMethods(unittest.TestCase):
     def tearDown(self):
         self.db.close()
 
-    def test_add(self):
-        self.db.add(key='hola', data=u'carnal!')
+    def test_add_found(self):
+        self.db.add(key='hola', data=u'mundo!')
         found, data = self.db.get('hola')
         assert found
-        assert data == 'carnal!'
+        assert data == 'mundo!'
+
+    def test_add_not_found(self):
+        found, data = self.db.get('hola')
+        assert not found
+        assert not data
+
+    def test_remove(self):
+        self.db.add(key='hola', data=u'mundo!')
+        self.db.remove('hola')
+        found, data = self.db.get('hola')
+        assert not found
+        assert not data
+
+    def test_change(self):
+        self.db.add(key='hola', data=u'mundo!')
+        found, data = self.db.get('hola')
+        assert found
+        assert data == 'mundo!'
+        self.db.add(key='hola', data=u'world!')
+        found, data = self.db.get('hola')
+        assert found
+        assert data == 'world!'
 
 if __name__ == '__main__':
     unittest.main()
