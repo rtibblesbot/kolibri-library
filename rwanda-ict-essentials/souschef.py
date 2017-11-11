27c98f5614bcfcf5e8fa396542926f2ccc35a3bb
Hermes Ojeda Ruiz <hermes.o.r@gmail.com>
2017-11-11 01:20:54 -0600

    Simple unit name extraction

--- rwanda-ict-essentials/souschef.py
+++ rwanda-ict-essentials/souschef.py
@@ -6,15 +6,16 @@ from utils import data_writer, path_builder, downloader
 from le_utils.constants import licenses, exercises, content_kinds, file_formats, format_presets, languages
 
 
-""" Additional imports """
+# Additional imports 
+###########################################################
+from utils.downloader import read
+from bs4 import BeautifulSoup
+
+# Run Constants
 ###########################################################
 
-
-""" Run Constants"""
-###########################################################
-
-CHANNEL_NAME = "TakeHomeTest"              # Name of channel
-CHANNEL_SOURCE_ID = "learningequality"      # Channel's unique id
+CHANNEL_NAME = "RME ICT Essentials for Teachers"              # Name of channel
+CHANNEL_SOURCE_ID = "rme-ict-essentials"      # Channel's unique id
 CHANNEL_DOMAIN = "content@learningequality.org"					# Who is providing the content
 CHANNEL_LANGUAGE = "en"		# Language of channel
 CHANNEL_DESCRIPTION = None                                  # Description of the channel (optional)
@@ -23,23 +24,29 @@ PATH = path_builder.PathBuilder(channel_name=CHANNEL_NAME)  # Keeps track of pat
 WRITE_TO_PATH = "{}{}{}.zip".format(os.path.dirname(os.path.realpath(__file__)), os.path.sep, CHANNEL_NAME) # Where to generate zip file
 
 
-""" Additional Constants """
+# Additional Constants 
 ###########################################################
+BASE_URL = "https://ict-essentials-for-teachers.moodlecloud.com/"
 
-
-""" Main Scraping Method """
+# Main Scraping Method 
 ###########################################################
 def scrape_source(writer):
-	""" scrape_source: Scrapes channel page and writes to a DataWriter
+    """ scrape_source: Scrapes channel page and writes to a DataWriter
         Args: writer (DataWriter): class that writes data to folder/spreadsheet structure
         Returns: None
     """
+    content = read(BASE_URL) 
 
-	# TODO: Replace line with scraping code
-	raise NotImplementedError("Scraping method not implemented")
+    soup = BeautifulSoup(content, 'html.parser')
+    for div in soup.find_all("div", class_= "coursename"):
+        link = div.find("a")
+        print(link.name)
+        print(link.get_text())
+    # TODO: Replace line with scraping code
+    raise NotImplementedError("Scraping method not implemented")
 
 
-""" Helper Methods """
+# Helper Methods 
 ###########################################################
 
 
