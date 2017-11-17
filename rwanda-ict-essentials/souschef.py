ed7666b2e50f14535e833ad850ab19da64116c8e
Hermes Ojeda Ruiz <hermes.o.r@gmail.com>
2017-11-16 23:34:04 -0600

    Fix indentation

--- rwanda-ict-essentials/souschef.py
+++ rwanda-ict-essentials/souschef.py
@@ -33,22 +33,29 @@ WRITE_TO_PATH = "{}{}{}.zip".format(os.path.dirname(os.path.realpath(__file__)),
 BASE_URL = "https://ict-essentials-for-teachers.moodlecloud.com/"
 
 IMG_LOOKUP = {
-	'SectionIntroduction%20%281%29.png': 'Introduction', 
-	'UnitSectionConclusion%20%281%29.png': 'Conclusion',
-	'SectionTime3%20%281%29.png': 'Recommended Time',
-	'SectionAttribution.png': 'Attribution',
-	'SectionActivity%20%281%29.png': 'Activity',
-	'SectiontMethod.png': 'Method',
-	'SectionIntroduction.png': 'Introduction',
-	'SectionFacilitation.png': 'Facilitator\' Welcome',
-	'SectionPortfolio.png': 'Portfolio assignment',
-	'SectionTime3.png': 'Recommended Time',
-	'SectionActivity.png': 'Activity',
-	'SectionIntroduction%20%283%29.png': 'Introduction',
-	'SectionTime3%20%282%29.png': 'Recommended Time',
-	'SectionIntroduction%20%282%29.png': 'Introduction',
-	'UnitReferences.png': 'References',
-	'SectionCompetency.png': 'Competency'
+       # 'UnitMethod1.png': '',  
+        'SectionObjective.png': 'Objective', 
+        'UnitSectionConclusion.png': 'Conclusion', 
+       # 'UnitMethod2.png': '', 
+       # 'UnitQuoteportfolio.png': '',    
+       # 'UnitQuizQuote.png': '', 
+       # 'UnitPolicyQuote.png': '',
+        'SectionIntroduction%20%281%29.png': 'Introduction', 
+        'UnitSectionConclusion%20%281%29.png': 'Conclusion',
+        'SectionTime3%20%281%29.png': 'Recommended Time',
+        'SectionAttribution.png': 'Attribution',
+        'SectionActivity%20%281%29.png': 'Activity',
+        'SectiontMethod.png': 'Method',
+        'SectionIntroduction.png': 'Introduction',
+        'SectionFacilitation.png': 'Facilitator\'s Welcome',
+        'SectionPortfolio.png': 'Portfolio assignment',
+        'SectionTime3.png': 'Recommended Time',
+        'SectionActivity.png': 'Activity',
+        'SectionIntroduction%20%283%29.png': 'Introduction',
+        'SectionTime3%20%282%29.png': 'Recommended Time',
+        'SectionIntroduction%20%282%29.png': 'Introduction',
+        'UnitReferences.png': 'References',
+        'SectionCompetency.png': 'Competency'
      }
 
 
@@ -60,17 +67,16 @@ name_of_files = set()
 ###########################################################
 def scrape_source(writer):
     """ scrape_source: Scrapes channel page and writes to a DataWriter
-	Args: writer (DataWriter): class that writes data to folder/spreadsheet structure
-	Returns: None
+        Args: writer (DataWriter): class that writes data to folder/spreadsheet structure
+        Returns: None
     """
     content = read(BASE_URL) 
 
     soup = BeautifulSoup(content, 'html.parser')
     units = get_units_from_site(soup)
     for u in units:
-	print(u['name'])  
+        print(u['name'])  
         parse_unit(writer, u['name'], u['link'])
-
     print(links)
     print("Number of links")
     print(len(links))
@@ -82,10 +88,10 @@ def scrape_source(writer):
     pattern = re.compile("bubble") 
     removed_strings = []
     for x in name_of_files:
-	if pattern.match(x):
-	    removed_strings.append(x) 
+        if pattern.match(x):
+            removed_strings.append(x) 
     for x in removed_strings:
-	name_of_files.remove(x)
+        name_of_files.remove(x)
 
     print(name_of_files)
     print(len(name_of_files))
@@ -100,9 +106,9 @@ def get_units_from_site(page):
     """
     units = []
     for div in page.find_all("div", class_= "coursename"):
-	link = div.find("a", text = re.compile("Unit"))
-	if link:
-	    units.append({ 'name': link.get_text(), 'link': link.get('href')})
+        link = div.find("a", text = re.compile("Unit"))
+        if link:
+            units.append({ 'name': link.get_text(), 'link': link.get('href')})
     return units
 
 def parse_unit(writer, name, link):
@@ -110,30 +116,30 @@ def parse_unit(writer, name, link):
       Parse the elements inside a unit
       Extract sections, and each section would be an independent HTML5App
     """
-    content = read(link) 
+    content = read(link)
     page = BeautifulSoup(content, 'html.parser')
     PATH.open_folder(folder_name(name))
 
     sections = page.find_all('li', id = re.compile('section-')) 
     writer.add_folder(str(PATH), name, "", "TODO: Generate Description")
     for section in sections:
-	title = generate_html5app_from_section(section)
-	print_modules(section)
-	writer.add_file(str(PATH), html5app_filename(title), html5app_path_from_title(title), license="TODO", copyright_holder = "TODO")
-	os.remove(html5app_path_from_title(title))
+        title = generate_html5app_from_section(section)
+        print_modules(section)
+        writer.add_file(str(PATH), html5app_filename(title), html5app_path_from_title(title), license="TODO", copyright_holder = "TODO")
+        os.remove(html5app_path_from_title(title))
     PATH.go_to_parent_folder()
     return 0
 
 def generate_html5app_from_section(section):
     page_title = section.find("h3", class_="sectionname")
-    title = "no title" 
-    if page_title: 
-	title = page_title.get_text()
+    title = "no title"
+    if page_title:
+        title = page_title.get_text()
     print("\t" + str(title) + " (" + section.get('id') + ")")
     html5app_filename = html5app_path_from_title(title)
     with HTMLWriter(html5app_filename) as html5zip:
-	content = section.encode_contents
-	html5zip.write_index_contents("<html><head></head><body>{}</body></html>".format(content))   
+        content = section.encode_contents
+        html5zip.write_index_contents("<html><head></head><body>{}</body></html>".format(content))   
     return title 
 
 def html5app_filename(title):
@@ -144,59 +150,65 @@ def html5app_path_from_title(title):
 
 def folder_name(unit_name):
     """ 
-  Extract folder name from full unit name
+    Extract folder name from full unit name
 
-  Example:
-  Unit 01 - Example unit
+    Example:
+    Unit 01 - Example unit
 
-  returns:
-  Unit 01 
-  """
-  return re.search('(.+?) - .*', unit_name).group(1)
+    returns:
+    Unit 01 
+    """
+    return re.search('(.+?) - .*', unit_name).group(1)
 
 def print_modules(section):
     global links
-  global name_of_files 
-  global added_links
-  modules = section.find_all("li", id=re.compile('module-'))
-  for module in modules:
-      titles = module.find_all("img", class_=re.compile("atto_image_button_text-[bottom|middle]"))
-    for t in titles:
-	print("\t\t - " + str(t.get('alt') + " " + clasify_module(module)))
-      if isValidTitle(t):
-	  added_links += 1
-	links.add(t.get("src"))
-	name_of_files.add(t.get("src").split("/")[-1])
-  if len(modules) == 0 :
-      print("\t\t - " + clasify_module(section))
-  return 0 
+    global name_of_files 
+    global added_links
+    modules = section.find_all("li", id=re.compile('module-'))
+    for module in modules:
+        titles = module.find_all("img", class_=re.compile("atto_image_button_"))
+        for t in titles:
+            # print("\t\t - " + str(real_title(t) + " " + clasify_module(module)))
+            # if real_title(t) == "Recommended Time":
+            #    print(t.parent.parent.get_text().strip())
+            if isValidTitle(t):
+                added_links += 1
+                links.add(t.get("src"))
+                name_of_files.add(t.get("src").split("/")[-1])
+    if len(modules) == 0 :
+       print("\t\t Unit Title - " + clasify_module(section))
+    return 0 
 
 def clasify_module(module):
-    module_type = "html"
-  video = module.find("iframe", src=re.compile("youtube"))
-  if video:
-      module_type = "\033[91m video \033[0m"
-  return module_type
+    module_type = "( html )"
+    video = module.find("iframe", src=re.compile("youtube"))
+    if video:
+        module_type = "\033[91m ( video ) \033[0m"
+    return module_type
+
+def real_title(title):
+    filename = title.get("src").split("/")[-1]
+    return IMG_LOOKUP[filename]
 
 def isValidTitle(title):
     """
-  isValidTitle is true if the name of the image contains Unit or Title on it 
-  """
-  pattern = re.compile("[Unit|Section]")
-  title = title.get("src").split("/")[-1]
-  return pattern.match(title)   
+    isValidTitle is true if the name of the image contains Unit or Title on it 
+    """
+    pattern = re.compile("(Unit|Section)^(Quote)", re.IGNORECASE)
+    filename = title.get("src").split("/")[-1]
+    return pattern.match(filename)   
 
-""" This code will run when the sous chef is called from the command line. """
-if __name__ == '__main__':
+    """ This code will run when the sous chef is called from the command line. """
+    if __name__ == '__main__':
 
-    # Open a writer to generate files
-    with data_writer.DataWriter(write_to_path=WRITE_TO_PATH) as writer:
+        # Open a writer to generate files
+        with data_writer.DataWriter(write_to_path=WRITE_TO_PATH) as writer:
 
-	# Write channel details to spreadsheet
-	thumbnail = writer.add_file(str(PATH), "Channel Thumbnail", CHANNEL_THUMBNAIL, write_data=False)
-	writer.add_channel(CHANNEL_NAME, CHANNEL_SOURCE_ID, CHANNEL_DOMAIN, CHANNEL_LANGUAGE, description=CHANNEL_DESCRIPTION, thumbnail=thumbnail)
+            # Write channel details to spreadsheet
+            thumbnail = writer.add_file(str(PATH), "Channel Thumbnail", CHANNEL_THUMBNAIL, write_data=False)
+            writer.add_channel(CHANNEL_NAME, CHANNEL_SOURCE_ID, CHANNEL_DOMAIN, CHANNEL_LANGUAGE, description=CHANNEL_DESCRIPTION, thumbnail=thumbnail)
 
-	# Scrape source content
-	scrape_source(writer)
+            # Scrape source content
+            scrape_source(writer)
 
-	sys.stdout.write("\n\nDONE: Zip created at {}\n".format(writer.write_to_path))
+    sys.stdout.write("\n\nDONE: Zip created at {}\n".format(writer.write_to_path))
