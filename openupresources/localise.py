e2f04a8553761195040f14f31c2ae3af2b0c8d9f
Dragon Dave McKee <dragon@scraperwiki.com>
2017-11-22 19:38:39 +0000

    Geogebra kinda working

--- openupresources/localise.py
+++ openupresources/localise.py
@@ -4,7 +4,9 @@ from urllib.parse import urljoin
 import hashlib
 import os
 import codecs
-
+from io import BytesIO
+from zipfile import ZipFile
+import geogebra
 session = requests.Session()
 
 class LocalConfig(object):
@@ -16,9 +18,14 @@ def get_resources(soup, config):
     def is_valid_tag(tag):
         if not any(link in tag.attrs for link in config.LINK_ATTRIBUTES):
             return False
+        # ig
         for combo in config.IGNORE_COMBOS:
             if tag.name==combo[0] and combo[1] in tag.attrs:
                 return False
+        # do not rewrite self-links
+        href = tag.attrs.get("href")
+        if href and href[0]== "#":
+            return False
         return True
             
     resources = set()
@@ -26,7 +33,7 @@ def get_resources(soup, config):
         l = soup.find_all(lambda tag: is_valid_tag(tag))
         resources.update(l)
     return resources
-    
+
 def make_local(page_url, config=None):
     # TODO 404 handling etc.
     # TODO remove 'foo'
@@ -41,6 +48,30 @@ def make_local(page_url, config=None):
     
     soup = BeautifulSoup(html_response.content, 'html.parser')
     resources = get_resources(soup, config)
+    # replace mathjax
+    # TODO: actually install it
+    mathjax, = [resource for resource in resources if 'src' in resource.attrs and "MathJax.js" in resource.attrs.get('src')]
+    resources.remove(mathjax)
+    mathjax.attrs['src'] = 'mathjax/MathJax.js'
+    
+    # replace geogebra
+    """<div class="geogebra-embed-wrapper" data-ggb_id="Vxv48Gtz">...</div>"""
+    geos = soup.find_all("div", {'class': 'geogebra-embed-wrapper'})
+    for geo in geos:
+        geo_id = geo.attrs['data-ggb_id']
+        # TODO - actually extract zip
+        data = geogebra.get_zip(geo_id)
+        geo_zip = BytesIO(data)
+        with ZipFile(geo_zip) as zip_file:
+            html_file, = [filename for filename in zip_file.namelist() if '/' not in filename and filename.endswith('.html')]
+        geo.name = "iframe"
+        geo.attrs['src'] = 'geogebra/{html_file}'.format(geo_id=geo_id, html_file=html_file)
+        
+        
+        
+        
+        
+    
     # note: ensure order of raw_url_list remains the same as other url_lists we later generate.
     # (hopefully there's not two different looking but identical urls)
     raw_url_list = [resource.attrs.get('href') or resource.attrs.get('src') for resource in resources]
@@ -63,9 +94,7 @@ def make_local(page_url, config=None):
         # str(soup) appears to contain utf-8: "\xe2\x80\x8b" [not bytes] -- soup was based on text at this point, not content
         
         f.write(str(soup))
-        
-        
-    raise RuntimeError("END")
     
 make_local("https://im.openupresources.org/7/students/5/3.html")
+print("END")
 
