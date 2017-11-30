7a043bebda614d7cc9e9487362f2e5a7016dc54f
Ivan Savov <ivan.savov@gmail.com>
2017-11-30 11:27:54 -0500

    using Db as context processor

--- tahrir-academy/test_youtube_utils.py
+++ tahrir-academy/test_youtube_utils.py
@@ -4,10 +4,7 @@ from youtube_utils.cache import Db
 from youtube_utils.youtube import Client, CachingClient
 
 
-cache = Db('.cache', 'tahrirchannels')
-yt = Client(youtube_dl.YoutubeDL(dict(verbose=True, no_warnings=True, writesubtitles=True, allsubtitles=True)))
-youtube = CachingClient(yt, cache)
-
-youtube.get_video_data('mOpCL3ggpCQ')
-
-
+with Db('.cache', 'tahrirchannels') as cache:
+    yt = Client(youtube_dl.YoutubeDL(dict(verbose=True, no_warnings=True, writesubtitles=True, allsubtitles=True)))
+    youtube = CachingClient(yt, cache)
+    print(youtube.get_video_data('mOpCL3ggpCQ'))
