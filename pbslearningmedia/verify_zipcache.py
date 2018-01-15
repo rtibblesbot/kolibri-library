d0cbdd003fdaaedfa51b47acbce1a24a410f08de
chef <chef@learningequality.org>
2018-01-15 08:48:25 -0800

    Check all zipfiles in cache OK

---
+++ pbslearningmedia/verify_zipcache.py
@@ -0,0 +1,11 @@
+import os
+all_files = os.listdir("zipcache")
+zip_files = [x for x in all_files if x.endswith(".zip")]
+bad = 0
+for filename in zip_files:
+    with open("zipcache/"+filename, "rb") as f:
+        read = f.read(2)[:2]
+        if read != b"PK":
+            print(filename, read)
+            bad = bad + 1
+print ("{} bad zip files found".format(bad))
