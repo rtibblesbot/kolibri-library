0d80b474ff1c08bd51ee4d287cf59d3ea851844c
Ivan Savov <ivan.savov@gmail.com>
2018-01-27 17:41:57 -0800

    updated run script

--- teachengineering/run.sh
+++ teachengineering/run.sh
@@ -1,33 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-#ARCHIVE_NAME="Teach Engineering"
+echo "1. RUNNING EN CHEF >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+./sushichef.py -v --reset --token=".token" lang=en
 
-#echo ""
-#echo "1. RUNNING SOUCHEF >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-#./souschef.py
-
-#if [ ! -f  "${ARCHIVE_NAME}".zip ]; then
-#  echo "Cannot find archive "${ARCHIVE_NAME}".zip in current directpry"
-#  echo "Please check variable ARCHIVE_NAME is properly set in ./run.sh script"
-#fi
+echo "2. RUNNING ES CHEF >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+./sushichef.py -v --reset --token=".token" lang=es
 
 
-
-#echo ""
-#echo "2. UNZIPPING ARCHIVE in content/ dir  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-#if [ ! -d  content ]; then
-#  mkdir content
-#fi
-#rm -rf content/*
-#mv "$ARCHIVE_NAME".zip content/
-#cd content
-#unzip -oq "$ARCHIVE_NAME".zip
-#cd ..
-
-
-
-
-echo ""
-echo "1. RUNNING JSON TREE CHEF >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-./sushichef.py -v --reset --token=".token"
