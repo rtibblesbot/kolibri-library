bf9e46f1545786bb67bb69b789cfd87a14cb5534
Dragon Dave McKee <dragon@scraperwiki.com>
2018-02-12 17:15:20 +0000

    Identify resources in carousel

--- artsedge/single_lesson.py
+++ artsedge/single_lesson.py
@@ -22,11 +22,14 @@ def handle_xml(xml_string):
     """
     lesson: has a tag 'label' like OVERVIEW, APPLY, ASSESS
     contains
-        module: with attrib 'type' like website, photo, overview.
+        module: with attrib 'type' [website/video/text/photo/overview/interactive/gallery/audio]
+        [1 interactive; a dozen or so of video/text/photo/audio/gallery]
+                -- 
                 which has its own labels, Character Map, etc.
                 
                 photo: <image><src>(url) ... has credits
                 website: <link><href>(url)
+                video: <download><href> or <resource><href>
                 ???: <resource>
                 
                 image | resource | download mutually exclusive
@@ -34,19 +37,30 @@ def handle_xml(xml_string):
                 
                 image: src
                 download, link: label, href
-                media: image, more critically src, chapters, subtitles, etc. -- complex!
+                media: image, more critically src, chapters, subtitles, etc. empty....
+                
+                tabs: rare (~6 total, appear to be image captions) IGNORE.
     
     """
     # proper XML handling requires 'lxml' to be installed [pip install lxml]
     
     soup = BeautifulSoup(xml_string, "xml")
     lessons = soup.find_all("lesson")
-    # lessons -> module...
     for lesson in lessons:
         modules = lesson.find_all("module")
         for module in modules:
             print (module.attrs['type'])
+            module_tags = []
             for tag in ['image', 'resource', 'download', 'link', 'media']:
-                print ("  ", module.find_all(tag, recursive=False))
+                module_tags.extend(module.find_all(tag, recursive=False))
+            for tag in module_tags:
+                src = tag.find("src", recursive=False)
+                href = tag.find("href", recursive=False)
+                if src:
+                    print ("  ", src.text)
+                if href:
+                    print ("  ", href.text)
+                assert not (src and href)
+                    
     
 handle_xml(xml_string)
