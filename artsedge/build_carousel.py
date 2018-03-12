ec769e8956f52dc3d6a01313745dcd5f3ab0f05a
Dragon Dave McKee <dragon@scraperwiki.com>
2018-03-12 16:35:43 +0000

    Carousel improvements

--- artsedge/build_carousel.py
+++ artsedge/build_carousel.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from bs4.element import Tag
 from urllib.parse import urlsplit
+
 filenames = """
    https://artsedge.kennedy-center.org/~/media/ArtsEdge/Images/LessonArt/grade-3-4/youre-invited-to-a-ceili-exploring-irish-dance.jpg
    https://artsedge.kennedy-center.org/~/media/ArtsEdge/Images/LessonArt/grade-3-4/ceili/irish_dance.ashx
@@ -15,41 +16,42 @@ filenames = """
 
 filenames = [x.strip() for x in filenames]
 
-with open("html/play_template.html", "rb") as f:
-    html_bytes = f.read()
+def create_carousel(filenames):
+    """Take a list of filenames and create a HTML5App.
+       It is not the job of this function to convert URLs to filenames!"""
 
-soup = BeautifulSoup(html_bytes, "html5lib")
+    with open("html/play_template.html", "rb") as f:
+        html_bytes = f.read()
 
-small_slick_replace, large_slick_replace = soup.findAll("divreplace")
+    soup = BeautifulSoup(html_bytes, "html5lib")
+    # note: this just uses the order in the HTML document -- not very stable
+    small_slick_replace, large_slick_replace = soup.findAll("divreplace")
 
+    def add_image(parent_tag, imgsrc, thumbnail=False):
+        div_tag = soup.new_tag("div")
+        img_tag = soup.new_tag("img")
+        img_tag.attrs['src'] = imgsrc
+        if thumbnail:
+            img_tag.attrs['width'] = 100
+            img_tag.attrs['height'] = 100
+        div_tag.insert(0, img_tag)
+        parent_tag.insert(0, div_tag)
 
+    # this could probably be tidied to remove the <placeholder> tag.
+    large_slick = soup.new_tag("placeholder")
+    for filename in reversed(filenames):
+        add_image(large_slick, filename)
 
-def add_image(parent_tag, imgsrc, thumbnail=False):
-    div_tag = soup.new_tag("div")
-    img_tag = soup.new_tag("img")
-    img_tag.attrs['src'] = imgsrc
-    if thumbnail:
-        img_tag.attrs['width'] = 100
-        img_tag.attrs['height'] = 100
-    div_tag.insert(0, img_tag)
-    parent_tag.insert(0, div_tag)
+    small_slick = soup.new_tag("placeholder")
+    for filename in reversed(filenames):
+        add_image(small_slick, filename, thumbnail=True)
 
+    large_slick_replace.replace_with(large_slick)
+    small_slick_replace.replace_with(small_slick)
+    large_slick.replaceWithChildren()
+    small_slick.replaceWithChildren()
 
-large_slick = soup.new_tag("placeholder")
-for filename in reversed(filenames):
-    add_image(large_slick, filename)
+    with open("html/play_output.html", "w") as f:
+        f.write(soup.prettify())
 
-
-small_slick = soup.new_tag("placeholder")
-for filename in reversed(filenames):
-    add_image(small_slick, filename, thumbnail=True)
-
-large_slick_replace.replace_with(large_slick)
-small_slick_replace.replace_with(small_slick)
-large_slick.replaceWithChildren()
-small_slick.replaceWithChildren()
-
-print (soup)
-
-with open("html/play_output.html", "w") as f:
-    f.write(soup.prettify())
+create_carousel(filenames)
