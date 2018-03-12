8b868a1a90fa0b6b45ef44ce3d156a7acbe561a3
Dragon Dave McKee <dragon@scraperwiki.com>
2018-03-12 18:54:59 +0000

    Add captions

--- artsedge/build_carousel.py
+++ artsedge/build_carousel.py
@@ -1,6 +1,7 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 from bs4.element import Tag
 from urllib.parse import urlsplit
+from itertools import zip_longest
 
 filenames = """
    https://artsedge.kennedy-center.org/~/media/ArtsEdge/Images/LessonArt/grade-3-4/youre-invited-to-a-ceili-exploring-irish-dance.jpg
@@ -15,19 +16,24 @@ filenames = """
 """.strip().split("\n")
 
 filenames = [x.strip() for x in filenames]
+captions = filenames
 
-def create_carousel(filenames):
+def create_carousel(filenames, captions=[]):
     """Take a list of filenames and create a HTML5App.
        It is not the job of this function to convert URLs to filenames!"""
 
     with open("html/play_template.html", "rb") as f:
         html_bytes = f.read()
 
+    if captions:
+        assert len(captions) == len(filenames), "Mismatch between filenames and captions length"
+
+    combined_data = list(zip_longest(reversed(filenames), reversed(captions)))
     soup = BeautifulSoup(html_bytes, "html5lib")
     # note: this just uses the order in the HTML document -- not very stable
     small_slick_replace, large_slick_replace = soup.findAll("divreplace")
 
-    def add_image(parent_tag, imgsrc, thumbnail=False):
+    def add_image(parent_tag, imgsrc, caption=None, thumbnail=False):
         div_tag = soup.new_tag("div")
         img_tag = soup.new_tag("img")
         img_tag.attrs['src'] = imgsrc
@@ -35,16 +41,18 @@ def create_carousel(filenames):
             img_tag.attrs['width'] = 100
             img_tag.attrs['height'] = 100
         div_tag.insert(0, img_tag)
+        if not thumbnail and caption:
+            div_tag.insert(0, NavigableString(caption))
         parent_tag.insert(0, div_tag)
 
     # this could probably be tidied to remove the <placeholder> tag.
     large_slick = soup.new_tag("placeholder")
-    for filename in reversed(filenames):
-        add_image(large_slick, filename)
+    for filename, caption in combined_data:
+        add_image(large_slick, filename, caption)
 
     small_slick = soup.new_tag("placeholder")
-    for filename in reversed(filenames):
-        add_image(small_slick, filename, thumbnail=True)
+    for filename, caption in combined_data:
+        add_image(small_slick, filename, caption, thumbnail=True)
 
     large_slick_replace.replace_with(large_slick)
     small_slick_replace.replace_with(small_slick)
@@ -54,4 +62,4 @@ def create_carousel(filenames):
     with open("html/play_output.html", "w") as f:
         f.write(soup.prettify())
 
-create_carousel(filenames)
+create_carousel(filenames, captions)
