abaae53c6369892e941b508f78b776df5ee5af38
Alejandro Martinez Romero <mara80@gmail.com>
2018-04-28 15:08:22 -0500

    added class video to download youtube videos

--- laboratoria/sushichef.py
+++ laboratoria/sushichef.py
@@ -8,18 +8,21 @@ import logging
 import markdown
 import ntpath
 import os
+import pafy
 from pathlib import Path
 import re
 import requests
-
 from ricecooker.classes.licenses import get_license
 from ricecooker.chefs import JsonTreeChef
 from ricecooker.utils import downloader, html_writer
 from ricecooker.utils.caching import CacheForeverHeuristic, FileCache, CacheControlAdapter
-#from urllib.error import URLError, urlencode
+from ricecooker.utils.jsontrees import write_tree_to_json_tree, SUBTITLES_FILE
+from urllib.error import URLError#, urlencode
 from urllib.parse import urljoin
 from utils import if_dir_exists, get_name_from_url, clone_repo, build_path
-from utils import if_file_exists
+from utils import if_file_exists, get_video_resolution_format, remove_links
+from utils import get_name_from_url_no_ext
+import youtube_dl   
 
 
 BASE_URL = "https://github.com/Laboratoria/"
@@ -32,6 +35,7 @@ __logging_handler = logging.StreamHandler()
 LOGGER.addHandler(__logging_handler)
 LOGGER.setLevel(logging.INFO)
 
+DOWNLOAD_VIDEOS = True
 
 sess = requests.Session()
 cache = FileCache('.webcache')
@@ -87,6 +91,13 @@ class Menu(object):
             pdf.download(path)
             yield pdf.to_node()
 
+    def write_videos(self):
+        path = [DATA_DIR] + self.page.pwd[2:]
+        path = build_path(path)
+        for video in self.page.get_videos():
+            video.download(download=DOWNLOAD_VIDEOS, base_path=path)
+            yield video.to_node()
+
     def to_node(self):
         pass
 
@@ -114,7 +125,7 @@ class Markdown(object):
     def load(self):
         self.content = self.parser(self.to_html())
         if self.content is not None:
-            self.get_copyright(self.content)
+            self.get_copyright()
 
     def to_html(self):
         try:
@@ -149,10 +160,19 @@ class Markdown(object):
 
     def get_pdfs(self):
         unique_urls = set([])
-        for a in self.content.findAll(lambda tag: tag.name == "a" and tag.attrs.get("href", "").endswith(".pdf")):
+        for a in self.content.find_all(lambda tag: tag.name == "a" and tag.attrs.get("href", "").endswith(".pdf")):
             url = a.get("href", "")
             if url not in unique_urls and url:
                 yield File(url, lang="es")
+                unique_urls.add(url)
+
+    def get_videos(self):
+        pattern = re.compile('youtube.com|youtu\.be')
+        unique_urls = set([])
+        for a in self.content.find_all("a", href=pattern):
+            url = a.get("href", "")
+            if url not in unique_urls and url:
+                yield YouTubeResource(url, lang="es")
                 unique_urls.add(url)
 
     def read_localdir(self):
@@ -166,8 +186,8 @@ class Markdown(object):
             LOGGER.info("Error: {}".format(e))
             return []
 
-    def get_copyright(self, content):
-        h2 = content.find(lambda tag: tag.name == "h2" and\
+    def get_copyright(self):
+        h2 = self.content.find(lambda tag: tag.name == "h2" and\
             tag.text.find("Copyright") != -1)
         if h2 is not None:
             self.copyright = h2.findNext('p')
@@ -178,7 +198,122 @@ class Markdown(object):
         menu.write_images()
         for node in menu.write_pdfs():
             node
+        for node in menu.write_videos():
+            node
+        remove_links(self.content)
         return menu
+
+
+
+class YouTubeResource(object):
+    def __init__(self, resource_url, type_name="Youtube", lang="en"):
+        LOGGER.info("Resource Type: "+type_name)
+        self.filename = None
+        self.type_name = type_name
+        self.filepath = None
+        self.resource_url = self.clean_url(resource_url)
+        self.file_format = file_formats.MP4
+        self.lang = lang
+
+    def clean_url(self, url):
+        if url[-1] == "/":
+            url = url[:-1]
+        return url.strip()
+
+    @classmethod
+    def is_youtube(self, url, get_channel=False):
+        youtube = url.find("youtube") != -1 or url.find("youtu.be") != -1
+        if get_channel is False:
+            youtube = youtube and url.find("user") == -1 and url.find("/c/") == -1
+        return youtube
+
+    @classmethod
+    def transform_embed(self, url):
+        url = "".join(url.split("?")[:1])
+        return url.replace("embed/", "watch?v=").strip()
+
+    def get_video_info(self):
+        ydl_options = {
+                'writesubtitles': True,
+                'allsubtitles': True,
+                'no_warnings': True,
+                'restrictfilenames':True,
+                'continuedl': True,
+                'quiet': False,
+                'format': "bestvideo[height<={maxheight}][ext=mp4]+bestaudio[ext=m4a]/best[height<={maxheight}][ext=mp4]".format(maxheight='720')
+            }
+
+        with youtube_dl.YoutubeDL(ydl_options) as ydl:
+            try:
+                ydl.add_default_info_extractors()
+                info = ydl.extract_info(self.resource_url, download=False)
+                return info
+            except(youtube_dl.utils.DownloadError, youtube_dl.utils.ContentTooShortError,
+                    youtube_dl.utils.ExtractorError) as e:
+                LOGGER.info('An error occured ' + str(e))
+                LOGGER.info(self.resource_url)
+            except KeyError as e:
+                LOGGER.info(str(e))
+
+    def subtitles_dict(self):
+        subs = []
+        video_info = self.get_video_info()
+        if video_info is not None:
+            video_id = video_info["id"]
+            if 'subtitles' in video_info:
+                subtitles_info = video_info["subtitles"]
+                for language in subtitles_info.keys():
+                    subs.append(dict(file_type=SUBTITLES_FILE, youtube_id=video_id, language=language))
+        return subs
+
+    #youtubedl has some troubles downloading videos in youtube,
+    #sometimes raises connection error
+    #for that I choose pafy for downloading
+    def download(self, download=True, base_path=None):
+        if not "watch?" in self.resource_url or "/user/" in self.resource_url or\
+            download is False:
+            return
+
+        download_to = build_path([base_path, 'videos'])
+        for try_number in range(10):
+            try:
+                video = pafy.new(self.resource_url)
+                best = get_video_resolution_format(video, maxvres=480, ext="mp4")
+                LOGGER.info("Video resolution: {}".format(best.resolution))
+                self.filepath = os.path.join(download_to, best.filename)
+                if not if_file_exists(self.filepath):
+                    self.filepath = best.download(filepath=download_to)
+                    self.filename = get_name_from_url_no_ext(self.filepath)
+                else:
+                    LOGGER.info("Already downloded: {}".format(self.filepath))
+                if os.stat(self.filepath).st_size == 0:
+                    LOGGER.info("Empty file")
+            except (ValueError, IOError, OSError, URLError, ConnectionResetError) as e:
+                LOGGER.info(e)
+                LOGGER.info("Download retry:"+str(try_number))
+                time.sleep(.8)
+            except (youtube_dl.utils.DownloadError, youtube_dl.utils.ContentTooShortError,
+                    youtube_dl.utils.ExtractorError, OSError) as e:
+                LOGGER.info("An error ocurred, may be the video is not available.")
+                return
+            except OSError:
+                return
+            else:
+                return
+
+    def to_node(self):
+        if self.filepath is not None:
+            files = [dict(file_type=content_kinds.VIDEO, path=self.filepath)]
+            files += self.subtitles_dict()
+            node = dict(
+                kind=content_kinds.VIDEO,
+                source_id=self.resource_url,
+                title=self.filename,
+                description='',
+                files=files,
+                language=self.lang,
+                license=get_license(licenses.CC_BY, copyright_holder=COPYRIGHT_HOLDER).as_dict())
+            return node
 
 
 class File(object):
--- laboratoria/utils.py
+++ laboratoria/utils.py
@@ -36,6 +36,16 @@ def get_name_from_url(url):
     return tail or basename
 
 
+def get_name_from_url_no_ext(url):
+    path = get_name_from_url(url)
+    path_split = path.split(".")
+    if len(path_split) > 1:
+        name = ".".join(path_split[:-1])
+    else:
+        name = path_split[0]
+    return name
+
+
 def clone_repo(git_url, repo_dir):
     if not if_dir_exists(repo_dir):
         Repo.clone_from(git_url, repo_dir)
@@ -46,3 +56,16 @@ def build_path(levels):
     if not if_dir_exists(path):
         os.makedirs(path)
     return path
+
+
+def get_video_resolution_format(video, maxvres=720, ext="mp4"):
+    formats = [(int(s.resolution.split("x")[1]), s.extension, s) for s in video.videostreams]
+    formats = sorted(formats, key=lambda x: x[0])
+    best = None
+    for r, x, stream in formats:
+        if r <= maxvres and x == ext:
+            best = stream
+    if best is None:
+        return video.getbest(preftype=ext)
+    else:
+        return best
