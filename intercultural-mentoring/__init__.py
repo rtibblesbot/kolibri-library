b0d694b36e37bc6c21553b0cfbc9a03f94876dc6
Jordan Yoshihara <jordanyoshihara@gmail.com>
2018-04-30 11:03:54 -0700

    Added cookiecutter files

---
+++ intercultural-mentoring/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for INTO Intercultural Mentoring chef."""
+
+__author__ = """Kevin Ollivier"""
+__email__ = 'kevin@learningequality.org'
+__version__ = '0.1.0'
