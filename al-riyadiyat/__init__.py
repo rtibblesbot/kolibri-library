cc7862e6145e5a177c3fa5103c42b02010fbd45d
Micah Fitch <fitchmicah@gmail.com>
2018-09-06 13:10:42 -0400

    initial commit

---
+++ al-riyadiyat/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Al-Riyadiyat with Basil Al-Zubaidi العربيّة chef."""
+
+__author__ = """Learning Equality"""
+__email__ = 'noreply@learningequality.org'
+__version__ = '0.1.0'
