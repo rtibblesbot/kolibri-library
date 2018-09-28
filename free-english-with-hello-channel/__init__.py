fbda758d9fd8cbea161ab1db72f9380522fbf9fc
kollivier <kevino@theolliviers.com>
2018-09-28 10:54:25 -0700

    Initial commit.

---
+++ free-english-with-hello-channel/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Free English with Hello Channel chef."""
+
+__author__ = """Alejandro Martinez Romero"""
+__email__ = 'mara80@gmail.com'
+__version__ = '0.1.0'
