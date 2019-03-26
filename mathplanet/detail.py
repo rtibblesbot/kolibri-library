7ae49a41ff4e56ee786ec9c590f2e05fe8e1edb3
Dragon Dave McKee <dragon@scraperwiki.com>
2019-03-26 18:33:34 +0000

    Get detail demoed

--- mathplanet/detail.py
+++ mathplanet/detail.py
@@ -2,15 +2,32 @@ import requests
 import requests_cache
 import lxml.html
 
+with open("template.html") as f:
+    template=f.read()
+
 class FakePage(object):
-    def __init__(self, url)
-    self.name = "[NAME: {}]".format(url)
-    self.url = url
+    def __init__(self, url):
+        self.name = "[NAME: {}]".format(url)
+        self.url = url
+    
+def get_video_node(url):
+    # TODO
+    return "Node", "sha1"
 
 def handle_lesson(page):
-    r = requests.get(page.url).content
+    video_nodes = []
+    images = []
+    html = requests.get(page.url).content
     root = lxml.html.fromstring(html)
-    article = root.xpath("//article[@id='article']")
+    article = root.xpath("//article[@id='article']")[0]
+    videos = article.xpath("//iframe")
+    images = article.xpath("//img")
+    for video in videos:
+        node, nodehash = get_video_node(video.attrib['src'])
+        video.attrib['src'] = "/content/storage/{}/{}/{}.mp4".format(nodehash[0], nodehash[1], nodehash)
+        
+    new_html = template.replace("{name}", page.name).replace("{article}", lxml.html.tostring(article).decode('utf-8'))
+    return new_html, video_nodes
     # acquire videos and images, make nice CSS
     # <iframe src="youtube"></iframe>
     
@@ -22,5 +39,5 @@ sample_urls = [
     ]
 
 for url in sample_urls:
-    handle_lesson(FakePage(url))
+    print(handle_lesson(FakePage(url)))
     
