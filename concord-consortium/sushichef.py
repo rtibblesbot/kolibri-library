d77838eff394fac910d44efaaefe1e6213653b45
Mark M <first.last@g-ail.c-m>
2019-04-11 13:17:21 -0700

    Get Concord resources sorted alphabetically

--- concord-consortium/sushichef.py
+++ concord-consortium/sushichef.py
@@ -174,7 +174,7 @@ def get_temp_dir():
 
 
 def get_all_resources():
-    api_search_url = 'https://learn.concord.org/api/v1/search/search?search_term=&sort_order=Newest&material_types%5B%5D=Investigation&material_types%5B%5D=Activity&material_types%5B%5D=Interactive&include_official=1&investigation_page=1&activity_page=1&interactive_page=1&per_page=1000'
+    api_search_url = 'https://learn.concord.org/api/v1/search/search?search_term=&sort_order=Alphabetical&material_types%5B%5D=Investigation&material_types%5B%5D=Activity&material_types%5B%5D=Interactive&include_official=1&investigation_page=1&activity_page=1&interactive_page=1&per_page=1000'
     api_response = requests.get(api_search_url)
     all_resources = json.loads(api_response.text)
 
