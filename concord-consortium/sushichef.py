dfca2903b39b42b5f75381ad85a206f4e7e54e0b
Mark M <first.last@g-ail.c-m>
2019-05-30 14:24:35 -0700

    Refactor: use more helper functions, rename vars

--- concord-consortium/sushichef.py
+++ concord-consortium/sushichef.py
@@ -72,73 +72,74 @@ class MyChef(SushiChef):
         """
         channel = self.get_channel(*args, **kwargs)  # Create ChannelNode from data in self.channel_info
 
-        models = get_all_resources()['models']
+        models = get_all_resources()['models'] # TODO activities and sequences
 
         preview_urls = list(map(lambda x: x['preview_url'], models))
         resolved_urls = list(map(lambda x: requests.get(x).url, preview_urls))
         parsed_urls = list(map(lambda x: urlparse(x), resolved_urls))
 
-        # Narrow down to just embeddable models for now
+        # Narrow down to just embeddable models for now # TODO all models, not just embeddable-based
         embeddable_parsed_urls = list(filter(lambda x: x.path == '/embeddable.html', parsed_urls))
         embeddable_base_urls = list(map(lambda x: x.scheme + '://' + x.netloc, embeddable_parsed_urls))
         embeddable_fragments = list(map(lambda x: x.fragment, embeddable_parsed_urls))
 
-        # Make parent directory ('temp') for all randomly-named temp dirs
-        temp_dir = get_temp_dir()
-        print(temp_dir)
-        print(os.path.basename(temp_dir))
+        # Get randomly-named temp dir to hold all the soup dirs for this run of the script
+        temp_dir = get_temp_dir(parent_dir='temp')
+        print('temp dir:', temp_dir)
 
-        # Download static assets for embeddable models into temp directory
         soups = [get_soup(e.geturl()) for e in embeddable_parsed_urls]
+        # Download assets for each embeddable-based model into own soup dir # TODO all models, not just embeddable-based
         for i, soup in enumerate(soups):
-            temp_subdir = temp_dir + os.sep + str(i)
-            os.makedirs(temp_subdir)
+            soup_dir = temp_dir + os.sep + 'soup_' + str(i).rjust(3, '0')
+            os.makedirs(soup_dir)
 
             print('\nDOWNLOADING STATIC ASSETS FOR SOUP', i)
             print('embeddable:', embeddable_parsed_urls[i].geturl())
 
-            doc = quietly(download_static_assets, soup, temp_subdir, embeddable_base_urls[i],
-                    url_blacklist=['analytics.js']) #TODO add ga.js to blacklist
+            doc = quietly(download_static_assets, soup, soup_dir, embeddable_base_urls[i],
+                    url_blacklist=['analytics.js']) # TODO add ga.js to blacklist
 
-            with open(temp_subdir + os.sep + 'index.html', 'w') as f:
+            # This file must be named index.html for Kolibri studio upload to work
+            with open(soup_dir + os.sep + 'index.html', 'w') as f:
                 fragment = embeddable_parsed_urls[i].fragment
-                f.write(str(doc).replace('document.location.hash', "'#" + fragment + "'"))
+                edited_doc_str = str(doc).replace('document.location.hash', "'#" + fragment + "'")
+                f.write(edited_doc_str)
 
-            # Save model json responses
+            # Save fragment-url json responses
             embeddable_fragment = embeddable_fragments[i]
-            fragment_file_dir = temp_subdir + os.sep + os.path.dirname(embeddable_fragment)
-            fragment_filename = os.path.basename(embeddable_fragment)
-            os.makedirs(fragment_file_dir)
+            download_dir = soup_dir + os.sep + os.path.dirname(embeddable_fragment)
+            filename = os.path.basename(embeddable_fragment)
 
             fragment_url = embeddable_base_urls[i] + os.sep + embeddable_fragment
-            fragment_json = json.loads(requests.get(fragment_url).text)
-            model_paths = []
-            with open(fragment_file_dir + os.sep + fragment_filename, 'w') as f:
+            fragment_json = requests.get(fragment_url).json()
+
+            os.makedirs(download_dir)
+            with open(download_dir + os.sep + filename, 'w') as f:
                 f.write(json.dumps(fragment_json))
-                if fragment_json.get('models'):
-                    for model in fragment_json['models']:
-                        if model.get('url'):
-                            model_paths.append(model['url'])
 
-            for j, model_path in enumerate(model_paths):
-                model_url = embeddable_base_urls[i] + os.sep + model_paths[j]
-                model_json = json.loads(requests.get(model_url).text)
+            if fragment_json.get('redirect'):
+                print('REDIRECT found')
+                # TODO: Deal with redirects
 
-                os.makedirs(temp_subdir + os.sep + os.path.dirname(model_path), exist_ok=True)
-                with open(temp_subdir + os.sep + model_path, 'w') as g:
-                    g.write(json.dumps(model_json))
+            relative_asset_paths = get_asset_paths_from_json(fragment_json)
+            for ap in relative_asset_paths:
+                print('JSON asset path:', ap)
+                asset_url = embeddable_base_urls[i] + os.sep + ap
+                asset_download_dir = soup_dir + os.sep + os.path.dirname(ap)
+                asset_filename = os.path.basename(ap)
+                download(asset_url, asset_download_dir + os.sep + asset_filename)
 
-            zip_path = create_predictable_zip(temp_subdir)
+            zip_path = create_predictable_zip(soup_dir)
 
             # topic_node_title = fragment_json.get('title', 'topic_title')
-            topic_node_title = fragment_json.get('title', 'topic_node_title_at_redirect') #TODO follow fragment_json redirects to get title
+            topic_node_title = fragment_json.get('title', 'follow redirect to get topic title') # TODO follow fragment_json redirects to get title
             topic_node_source_id = 'model/' + str(models[i]['id']) # str() in case it's an array with multiple 'about's
             topic_node = nodes.TopicNode(title=topic_node_title, source_id=topic_node_source_id)
             channel.add_child(topic_node)
 
             app_node_source_id = 'embeddable/' + os.path.splitext(os.path.basename(embeddable_fragment))[0]
-            app_node_title = fragment_json.get('title', 'app_node_title_at_redirect') #TODO follow fragment_json redirects to get title
-            app_node_description = str(fragment_json.get('about', 'app_node_description_at_redirect')) # str() in case it's an array with multiple 'about's #TODO follow fragment_json redirects to get title
+            app_node_title = fragment_json.get('title', 'follow redirect to get app title') # TODO follow fragment_json redirects to get title
+            app_node_description = str(fragment_json.get('about', 'follow redirect to get app description')) # str() in case it's an array with multiple 'about's # TODO deal with arrays # TODO follow fragment_json redirects to get title
             license = get_model_license(models[i])
             topic_node.add_child(nodes.HTML5AppNode(
                 source_id=app_node_source_id,
@@ -153,6 +154,41 @@ class MyChef(SushiChef):
         raise_for_invalid_channel(channel)  # Check for errors in channel construction
 
         return channel
+
+
+def get_asset_paths_from_json(json):
+    asset_paths = []
+
+    for model in json.get('models', []):
+        if model.get('url'):
+            asset_paths.append(model['url'])
+
+    if json.get('i18nMetadata'):
+        asset_paths.append(json['i18nMetadata'])
+
+    # TODO: metadata and locale data, svgs?
+
+    return asset_paths
+
+
+def download(url, download_path):
+    """ Expecting just JSON files (for now) """
+    response = requests.get(url)
+    if not response.ok:
+        print('FAILED TO DOWNLOAD, status_code:', response.status_code)
+        print('url:', url)
+        return
+    content_type = response.headers['content-type']
+    if not content_type.startswith('application/json'):
+        print('FAILED TO DOWNLOAD, wrong content-type:', content_type)
+        print('url:', url)
+        return
+
+    os.makedirs(os.path.dirname(download_path), exist_ok=True)
+    with open(download_path, 'w') as f:
+        f.write(json.dumps(response.json()))
+    # TODO: deal with other file types if needed
+    return
 
 
 def quietly(func, *args, **kwargs):
@@ -171,9 +207,9 @@ def get_model_license(model):
         return 'unknown_license'
 
 
-def get_temp_dir():
-    os.makedirs('temp', exist_ok=True)
-    temp_dir_prefix = os.getcwd() + os.sep + 'temp' + os.sep
+def get_temp_dir(parent_dir):
+    os.makedirs(parent_dir, exist_ok=True)
+    temp_dir_prefix = os.getcwd() + os.sep + parent_dir + os.sep
     temp_dir = tempfile.mkdtemp(prefix=temp_dir_prefix)
     return temp_dir
 
