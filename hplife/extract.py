ef1e715097a073034813b38ca89779f364b3fff8
Ivan Savov <ivan.savov@gmail.com>
2019-07-09 17:20:43 -0400

    Added Renamed logic -- handle : in course names and special subfolder case

--- hplife/extract.py
+++ hplife/extract.py
@@ -3,7 +3,7 @@ import io
 import json
 import os
 import pickle
-# import tarfile
+import re
 import shutil
 
 
@@ -66,7 +66,7 @@ def get_service(service_name=None, service_version=None):
 
 FOLDER_MIMETYPE = 'application/vnd.google-apps.folder'
 DEFAULT_FILE_FIELDS = 'id,kind,name,mimeType,version,webViewLink,createdTime,modifiedTime'
-FORBIDDEN_CHARS_IN_FOLDER_NAMES = ['/', ':']  # will be replaced with space ' '
+FORBIDDEN_CHARS_IN_FOLDER_NAMES = ['/', ':']  # will be replaced with underscore
 
 def itercontents(drive, folder_id, order_by='folder,name', file_fields=DEFAULT_FILE_FIELDS):
     """
@@ -103,7 +103,7 @@ def list_folder(folder_id):
 def _clean_folder_name(name):
     """Remove forbidden chars from folder name and strip any whitespace."""
     for char in FORBIDDEN_CHARS_IN_FOLDER_NAMES:
-        name = name.replace(char, ' ')
+        name = name.replace(char, '_')
     name = name.strip()
     return name
 
@@ -164,7 +164,22 @@ def gdrive_download_file(file_id, destpath, drive=None):
 # EXPORT
 ################################################################################
 
-EXPORT_DIRNAME = 'Export'
+EXPORTED_DIRNAME = 'Exported'
+
+def get_exported_dir(lang, kind):
+    """
+    Get the local path to the Exported directory for `lang`.
+    if kind==courses:       `chefdata/Exported/{lang}/{Language}`
+    if kind==activityfiles: `chefdata/Exported/{lang}/{Language} - Activity Files`
+    """
+    assert kind in ['courses', 'activityfiles']
+    exportdir = os.path.join('chefdata', EXPORTED_DIRNAME, lang)
+    data_sources = json.load(open('chefdata/data_sources.json'))
+    lang_data_sources = data_sources[lang]
+    dirname = lang_data_sources[kind]['name']
+    srcdir = os.path.join(exportdir, dirname)
+    return srcdir
+
 
 def export_folder(folder_id, parentdir='', drive=None):
     if not os.path.exists(parentdir):
@@ -198,7 +213,7 @@ def export(lang='all'):
 
     drive = get_service(service_name='drive', service_version='v3')
 
-    exportdir = os.path.join('chefdata', EXPORT_DIRNAME)
+    exportdir = os.path.join('chefdata', EXPORTED_DIRNAME)
     data_sources = json.load(open('chefdata/data_sources.json'))
 
     for lang in langs:
@@ -213,14 +228,134 @@ def export(lang='all'):
         export_folder(courses['folder_id'], parentdir=langdir, drive=drive)
 
         # export course activity files
-        content = lang_data_sources['content']
+        content = lang_data_sources['activityfiles']
         export_folder(content['folder_id'], parentdir=langdir, drive=drive)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# RENAME + STANDARDIZE
+################################################################################
+
+RENAMED_DIRNAME = 'Renamed'
+
+def get_renamed_dir(lang, kind):
+    """
+    Get the local path to the Renamed directory for `lang`.
+    if kind==courses:       `chefdata/Renamed/{lang}/{Language}`
+    if kind==activityfiles: `chefdata/Renamed/{lang}/{Language} - Activity Files`
+    """
+    assert kind in ['courses', 'activityfiles']
+    renameddir = os.path.join('chefdata', RENAMED_DIRNAME, lang)
+    data_sources = json.load(open('chefdata/data_sources.json'))
+    lang_data_sources = data_sources[lang]
+    dirname = lang_data_sources[kind]['name']
+    srcdir = os.path.join(renameddir, dirname)
+    return srcdir
+
+
+def _strip_course_number(name):
+    """
+    Removes the prefix of the form HPL-FR33 followed by : _ or space.
+    """
+    couse_number_pat = re.compile('HPL-..\d\d[: _]')
+    m = couse_number_pat.search(name)
+    if m:
+        clean_name = couse_number_pat.sub('', name)
+        return clean_name
+    else:
+        return name
+
+
+def _normalize_course_name(course_name):
+    """
+    Do name normalization to fix non-standard names containing _ and :
+    """
+    HPLIFE_COURSE_FOLDER_RENAMES = {
+        'Energy efficiency_ Do more for less': 'Energy efficiency - Do more for less',
+        'Eficiencia de la energía hacer más con menos': 'Eficiencia de la energía - hacer más con menos',
+        'Eficiencia de la energía_ hacer más con menos': 'Eficiencia de la energía - hacer más con menos',
+        'Efficacité énergétique Faire davantage avec moins': 'Efficacité énergétique - Faire davantage avec moins',
+        'Efficacité énergétique _ Faire davantage avec moins': 'Efficacité énergétique - Faire davantage avec moins',
+    }
+    for source_str, replacement_str in HPLIFE_COURSE_FOLDER_RENAMES.items():
+        if source_str in course_name:
+            course_name = course_name.replace(source_str, replacement_str)
+            print('Did normalization edit on course_name', course_name)
+    return course_name
+
+
+def rename_courses(lang):
+    """
+    Standardize on course names that have : or '  ' in them.
+    """
+    # source directory = `chefdata/Export/{lang}/{Language}/`
+    srcdir = get_exported_dir(lang, 'courses')
+    for filename in os.listdir(srcdir):
+        srcpath = os.path.join(srcdir, filename)
+        #
+        # Case A. Handle special course folder case
+        if os.path.isdir(srcpath):
+            print('Found course dir', srcpath)
+            course_name = _strip_course_number(filename)
+            childfilenames = os.listdir(srcpath)
+            assert len(childfilenames) == 1, 'errror multiple archives found in ' + srcpath
+            childfilename = childfilenames[0]
+            srcpath = os.path.join(srcdir, filename, childfilename)
+        #
+        # Case B: handle normaal case where course is a .gz or .tar.gz file
+        else:
+            course_name = _strip_course_number(filename)
+            if course_name.endswith('.tar.gz'):
+                course_name = course_name.replace('.tar.gz', '')
+            elif course_name.endswith('.gz'):
+                course_name = course_name.replace('.gz', '')
+            else:
+                print('unexpected filename', filename)
+        #
+        course_name = _normalize_course_name(course_name)
+        #
+        # dest
+        # dest directory = `chefdata/Renamed/{lang}/{Language}/`
+        
+        destdir = get_renamed_dir(lang, 'courses')
+        destpath = os.path.join(destdir, course_name + '.tar.gz')
+        #
+        # do copy
+        shutil.copy(srcpath, destpath)
+        print('Copied course', filename, 'to', destpath)
+
+
+def rename_activity_files(lang):
+    # source directory = chefdata/Export/{lang}/{Language} - Activity Files/
+    srcdir = get_exported_dir(lang, 'activityfiles')
+    # dest directory = chefdata/Renamed/{lang}/{Language} - Activity Files/
+    destdir = get_renamed_dir(lang, 'activityfiles')
+    # rename activity folders
+    for filename in os.listdir(srcdir):
+        srcpath = os.path.join(srcdir, filename)
+        course_name = _normalize_course_name(filename)
+        destpath = os.path.join(destdir, course_name)
+        shutil.copy(srcpath, destpath)
+
 
 
 
 
 # EXTRACT
 ################################################################################
+
 
 EXTRACT_DIRNAME = 'Courses'
 EXPORT_SUFFIX_TO_STRIP = ' - Storyline output'
@@ -229,22 +364,15 @@ FILES_TO_SKIP = ['.DS_Store', 'Thumbs.db', 'ehthumbs.db', 'ehthumbs_vista.db', '
 
 def extract_courses(lang):
     """
-    Extract all the `.gz`s from `chedata/Export/{lang}/{Langname}/{course_name}.gz`
+    Extract all the `.gz`s from `chefdata/Renamed/{lang}/{Langname}/{course_name}.gz`
     to `chefdata/Courses/{lang}/{course_name}/course`.
     Returns course_names = list of course names encountered.
     """
     course_names = []
-
     # src
-    exportdir = os.path.join('chefdata', EXPORT_DIRNAME, lang)
-    data_sources = json.load(open('chefdata/data_sources.json'))
-    lang_data_sources = data_sources[lang]
-    dirname = lang_data_sources['courses']['name']
-    srcdir = os.path.join(exportdir, dirname)
-
+    srcdir = get_renamed_dir(lang, 'courses')
     # dest
     extractdir = os.path.join('chefdata', EXTRACT_DIRNAME, lang)
-
     for filename in os.listdir(srcdir):
         if filename.endswith('.gz') or filename.endswith('.tar.gz'):
             gzpath = os.path.join(srcdir, filename)
@@ -267,18 +395,14 @@ def extract_courses(lang):
 
 def process_content_for_course(lang, course_name):
     """
-    Copy over all resource folders `chedata/Export/{lang}/{Langname} - Activity Files/{course_name}/{srcfolder}`
+    Copy over all resource folders `chedata/Renamed/{lang}/{Langname} - Activity Files/{course_name}/{srcfolder}`
     to `chefdata/Courses/{lang}/{course_name}/content/{srcfolder.replace(' - Storyline output','')}`
     Returns `activity_refs` (list) for resource folder names copied over.
     """
     activity_refs = []
 
     # src
-    exportdir = os.path.join('chefdata', EXPORT_DIRNAME, lang)
-    data_sources = json.load(open('chefdata/data_sources.json'))
-    lang_data_sources = data_sources[lang]
-    dirname = lang_data_sources['content']['name']
-    srcdir = os.path.join(exportdir, dirname)
+    srcdir = get_renamed_dir(lang, 'activityfiles')
     coursedir = os.path.join(srcdir, course_name)
     if not os.path.exists(coursedir):
         print('Could not find Activity-Files for course_name', course_name)
@@ -287,7 +411,6 @@ def process_content_for_course(lang, course_name):
     # dest
     extractdir = os.path.join('chefdata', EXTRACT_DIRNAME, lang)
     contentdir = os.path.join(extractdir, course_name, 'content')
-
 
     for srcfolder in os.listdir(coursedir):
         # src
@@ -325,5 +448,9 @@ def extract(lang):
 ################################################################################
 
 if __name__ == '__main__':
-    export(lang='es')
-    extract(lang='es')
+    from sushichef import HPLIFE_LANGS
+    for lang in HPLIFE_LANGS:
+        export(lang=lang)
+        rename_courses(lang=lang)
+        rename_activity_files(lang=lang)
+        extract(lang=lang)
