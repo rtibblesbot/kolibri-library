08a159a2a874e2c9b2e6032a0c86d287722fd627
Benjamin Bach <benjamin@overtag.dk>
2020-04-02 16:06:47 +0200

    Initial cookiecutter template

---
+++ elejandria-libros/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Elejandria Libros chef."""
+
+__author__ = """Learning Equality"""
+__email__ = 'benjamin@learningequality.org'
+__version__ = '0.1.0'
