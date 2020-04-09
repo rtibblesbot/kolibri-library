02ba29b7e96e5189a97f216e19f343b8803920ae
Benjamin Bach <benjamin@overtag.dk>
2020-04-09 21:55:23 +0200

    Opted for downloading everything sequentially at the end

--- elejandria-libros/sushichef.py
+++ elejandria-libros/sushichef.py
@@ -20,12 +20,14 @@ from scrapy.utils.log import configure_logging
 
 # Run constants
 ################################################################################
-CHANNEL_NAME = "Elejandria Libros"              # Name of channel
-CHANNEL_SOURCE_ID = "elejandria-libros"    # Channel's unique id
-CHANNEL_DOMAIN = "elejandria.com"          # Who is providing the content
-CHANNEL_LANGUAGE = "es"      # Language of channel
+CHANNEL_NAME = "Elejandria Libros"  # Name of channel
+CHANNEL_SOURCE_ID = "elejandria-libros"  # Channel's unique id
+CHANNEL_DOMAIN = "elejandria.com"  # Who is providing the content
+CHANNEL_LANGUAGE = "es"  # Language of channel
 CHANNEL_DESCRIPTION = "Elejandria es un sitio web que ofrece libros gratis de dominio público o publicados bajo licencias abiertas. La mayoría de los autores son clásicos de la literatura unviersal, pero también podrás descargar gratis libros de dominio público actuales con licencias de libre distribución."
-CHANNEL_THUMBNAIL = "channel_thumbnail.png"                                    # Local path or url to image file (optional)
+CHANNEL_THUMBNAIL = (
+    "channel_thumbnail.png"  # Local path or url to image file (optional)
+)
 
 # Additional constants
 ################################################################################
@@ -49,7 +51,25 @@ setup_logging(
 NODE_TREE = []
 
 # Maps nodes to URLs that should be downloaded and added after crawling
-NODE_DOWNLOAD_MAP = {}
+DOWNLOAD_JOBS = []
+
+
+class DownloadJob:
+    def __init__(self, node, file_cls, url):
+        """
+        node: the DocumentNode to create File object on
+        file_cls: a class, either DocumentFile or EPubFile
+        url: where to get the file from
+        """
+        self.file_cls = file_cls
+        self.url = url
+        self.node = node
+
+    def download(self):
+        logger.debug("Now downloading {}".format(self.url))
+        save_path = download_file(self.url)
+        instance = self.file_cls(save_path)
+        self.node.add_file(instance)
 
 
 def download_file(location, destdir=DOWNLOADED_FILES_DIR):
@@ -63,15 +83,15 @@ def download_file(location, destdir=DOWNLOADED_FILES_DIR):
     filename_base = location.split("/")[-1]
 
     if response.status_code == 200:
-        
+
         # TODO: Can this be shortened?
         _, params = cgi.parse_header(response.headers["Content-Disposition"])
         original_filename = params["filename"]
         filename_ext = original_filename.split(".")[-1]
         destination_filename = ".".join((filename_base, filename_ext))
-        
+
         logger.debug("Fetching {} and storing as {}")
-        
+
         out_path = os.path.join(destdir, destination_filename)
 
         if DEBUG and os.path.exists(out_path):
@@ -79,12 +99,14 @@ def download_file(location, destdir=DOWNLOADED_FILES_DIR):
         else:
             with open(out_path, "wb") as outf:
                 shutil.copyfileobj(response.raw, outf)
-            file_size_mb = os.path.getsize(out_path) / 1024.0 / 1024.0,
+            file_size_mb = os.path.getsize(out_path) / 1024.0 / 1024.0
             logger.info("Saved file {} of size {} MB".format(out_path, file_size_mb))
         return out_path
 
     else:
-        logger.warning("HTTP status {}, downloading: {}".format(response.status_code, location))
+        logger.warning(
+            "HTTP status {}, downloading: {}".format(response.status_code, location)
+        )
         return None
 
 
@@ -109,17 +131,12 @@ class ElejandriaLibrosSpider(scrapy.Spider):
             title = collection_link.css("::text").get()
             url = collection_link.attrib["href"]
             logger.debug("Found collection with title: {}".format(title))
-            ricecooker_node = nodes.TopicNode(
-                title=title,
-                source_id=url,
-            )
-            NODE_TREE.append(
-                ricecooker_node
-            )
+            ricecooker_node = nodes.TopicNode(title=title, source_id=url,)
+            NODE_TREE.append(ricecooker_node)
             request = scrapy.Request(
                 collection_link.attrib["href"],
                 callback=self.parse_collection,
-                cb_kwargs={'node': ricecooker_node}
+                cb_kwargs={"node": ricecooker_node},
             )
             yield request
 
@@ -128,18 +145,16 @@ class ElejandriaLibrosSpider(scrapy.Spider):
         Example:
         https://www.elejandria.com/coleccion/descargar-gratis-20-libros-clasicos-para-sobrellevar-la-cuarentena
         """
-        logger.debug("Parsing collection \"{}\": {}".format(node.title, response.url))
+        logger.debug('Parsing collection "{}": {}'.format(node.title, response.url))
         for book_link in response.css(".book div p a.primary-text-color"):
             url = book_link.attrib["href"]
             title = book_link.css("::text").get()
             logger.debug("Found book link: {} - ".format(title))
             request = scrapy.Request(
-                url,
-                callback=self.parse_book,
-                cb_kwargs={'node': node}
+                url, callback=self.parse_book, cb_kwargs={"node": node}
             )
             yield request
-            
+
     def parse_book(self, response, node):
         """
         Example:
@@ -150,14 +165,16 @@ class ElejandriaLibrosSpider(scrapy.Spider):
         book_title = response.css("h1.bordered-heading::text").get()
         author = response.css("h2 a.secondary-text-color::text").get()
         thumbnail = response.css("img.img-book-cover::attr(src)").get()
-        
+
         # Fetch a description based on all the text in P containers
         # There aren't really any semantic tags around this, so it's
         # probably going to break some day...
         description = "\n\n".join(
-            response.css("div.col-lg-8 div.row div.offset-top div.text-justify p::text").getall()
+            response.css(
+                "div.col-lg-8 div.row div.offset-top div.text-justify p::text"
+            ).getall()
         )
-        
+
         document_node = nodes.DocumentNode(
             source_id=response.url,
             title=book_title,
@@ -167,24 +184,28 @@ class ElejandriaLibrosSpider(scrapy.Spider):
             description=description,
             thumbnail=thumbnail,
             derive_thumbnail=True,
-            files=[]
+            files=[],
         )
         node.add_child(document_node)
-        
+
         versions = {}
         for download_button in response.css("a.download-link"):
             button_text = download_button.css("::text").get() or ""
             url = download_button.attrib["href"]
             if "ePub" in button_text:
-                versions['ePub'] = url
+                versions["ePub"] = url
             elif "PDF" in button_text:
-                versions['PDF'] = url
-        
+                versions["PDF"] = url
+
         # Prefer ePub, fall back to PDF
+        file_cls = None
+        url = None
         if "ePub" in versions:
-            url = versions['ePub']
+            url = versions["ePub"]
+            file_cls = files.EPubFile
         elif "PDF" in versions:
-            url = versions['PDF']
+            url = versions["PDF"]
+            file_cls = files.DocumentFile
         else:
             logger.error("No PDF or ePub version found: {}".format(response.url))
             return
@@ -192,17 +213,17 @@ class ElejandriaLibrosSpider(scrapy.Spider):
         request = scrapy.Request(
             url,
             callback=self.parse_download,
-            cb_kwargs={'node': document_node}
+            cb_kwargs={"node": document_node, "file_cls": file_cls},
         )
         yield request
 
-    def parse_download(self, response, node):
+    def parse_download(self, response, node, file_cls):
         logger.debug("Downloading ePub: {}".format(node.title, response.url))
         url = response.css(".book-description a.download-link::attr(href)").get()
         if not url:
             logger.error("Could not find download link: {}".format(url))
         else:
-            NODE_DOWNLOAD_MAP[node] = url
+            DOWNLOAD_JOBS.append(DownloadJob(node, file_cls, url))
 
 
 # The chef subclass
@@ -222,18 +243,14 @@ class ElejandriaLibrosChef(SushiChef):
         to build the contentnode tree.
     For more info, see https://github.com/learningequality/ricecooker/tree/master/docs
     """
-    channel_info = {                                   # Channel Metadata
-        'CHANNEL_SOURCE_DOMAIN': CHANNEL_DOMAIN,       # Who is providing the content
-        'CHANNEL_SOURCE_ID': CHANNEL_SOURCE_ID,        # Channel's unique id
-        'CHANNEL_TITLE': CHANNEL_NAME,                 # Name of channel
-        'CHANNEL_LANGUAGE': CHANNEL_LANGUAGE,          # Language of channel
-        'CHANNEL_THUMBNAIL': CHANNEL_THUMBNAIL,        # Local path or url to image file (optional)
-        'CHANNEL_DESCRIPTION': CHANNEL_DESCRIPTION,    # Description of the channel (optional)
+    channel_info = {  # Channel Metadata
+        "CHANNEL_SOURCE_DOMAIN": CHANNEL_DOMAIN,  # Who is providing the content
+        "CHANNEL_SOURCE_ID": CHANNEL_SOURCE_ID,  # Channel's unique id
+        "CHANNEL_TITLE": CHANNEL_NAME,  # Name of channel
+        "CHANNEL_LANGUAGE": CHANNEL_LANGUAGE,  # Language of channel
+        "CHANNEL_THUMBNAIL": CHANNEL_THUMBNAIL,  # Local path or url to image file (optional)
+        "CHANNEL_DESCRIPTION": CHANNEL_DESCRIPTION,  # Description of the channel (optional)
     }
-    # Your chef subclass can override/extend the following method:
-    # get_channel: to create ChannelNode manually instead of using channel_info
-    # pre_run: to perform preliminary tasks, e.g., crawling and scraping website
-    # __init__: if need to customize functionality or add command line arguments
 
     def crawl(self, args, options):
         # Invoke Scrapy this way -- it's the only way to avoid it from
@@ -244,12 +261,9 @@ class ElejandriaLibrosChef(SushiChef):
         d.addBoth(lambda _: reactor.stop())
         reactor.run()
 
-        for node, url in NODE_DOWNLOAD_MAP.items():
-            downloaded_path = download_file(url)
-            if downloaded_path:
-                node.add_file(
-                    downloaded_path
-                )
+    def scrape(self, args, options):
+        for job in DOWNLOAD_JOBS:
+            job.download()
 
     def transform(self, args, options):
         pass
@@ -260,7 +274,7 @@ class ElejandriaLibrosChef(SushiChef):
         #     if not os.path.exists(dir):
         #         os.makedirs(dir, exist_ok=True)
         self.crawl(args, options)
-        # self.scrape(args, options)
+        self.scrape(args, options)
         self.transform(args, options)
 
     def construct_channel(self, *args, **kwargs):
@@ -273,7 +287,9 @@ class ElejandriaLibrosChef(SushiChef):
             "fr" will be passed along to `construct_channel` as kwargs['lang'].
         Returns: ChannelNode
         """
-        channel = self.get_channel(*args, **kwargs)  # Create ChannelNode from data in self.channel_info
+        channel = self.get_channel(
+            *args, **kwargs
+        )  # Create ChannelNode from data in self.channel_info
 
         for node in NODE_TREE:
             channel.add_child(node)
@@ -286,10 +302,9 @@ class ElejandriaLibrosChef(SushiChef):
         # return channel
 
 
-
 # CLI
 ################################################################################
-if __name__ == '__main__':
+if __name__ == "__main__":
     # This code runs when sushichef.py is called from the command line
     chef = ElejandriaLibrosChef()
     chef.main()
