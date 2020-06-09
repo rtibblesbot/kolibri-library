f42770efefc6bac8ed5d6f014a015452ad3bbdb3
Jordan Yoshihara <jordanyoshihara@gmail.com>
2020-06-09 15:36:09 -0700

    Got all content types working

--- who-covid-advice/scrapers/who.py
+++ who-covid-advice/scrapers/who.py
@@ -1,63 +1,189 @@
+import os
+
 from bs4 import BeautifulSoup
-
-from ricecooker.classes import nodes
-
-
-class WHOPageScraperBase:
-    def __init__(self, url, file_on_disk):
-        self.url = url
-        self.file_on_disk = file_on_disk
-
-    def node_for_text_section(self, content):
-        # print("content = {}".format(content.prettify()))
-        pass
-
-    def node_for_video(self, content):
-        pass
-
-    def node_for_rows(self, rows):
-        for row in rows:
-            pass #print("row = {}".format(row.prettify()))
-
-    def get_ricecooker_node(self):
-        """
-        Convert the data at the URL to a ricecooker/Kolibri-compatible representation.
-        :return: A ricecooker.TreeNode-derived Node object.
-        """
-
-        raise NotImplementedError("Not implemented!")
+from le_utils.constants import content_kinds
+from ricecooker.classes import nodes, files
+from webmixer.scrapers.pages.base import PDFScraper, HTMLPageScraper, VideoScraper, WebVideoScraper
+from webmixer.scrapers.pages.fullscreen import FullscreenPageScraper
+from webmixer.scrapers.tags import LinkTag
+from webmixer.utils import get_absolute_url
 
 
-class WHOCovidAdvicePageScraper(WHOPageScraperBase):
-    def get_ricecooker_node(self):
-        soup = BeautifulSoup(open(self.file_on_disk).read())
+class ContentNodeMixin:
+    def to_contentnode(self, title, directory=None, *args, **kwargs):
+        filepath = self.to_file(directory=directory)
+        if self.kind == content_kinds.HTML5:
+            return nodes.HTML5AppNode(
+                source_id=self.url,
+                title=title,
+                files=[files.HTMLZipFile(filepath)],
+                **kwargs
+            )
+        elif self.kind == content_kinds.VIDEO:
+            return nodes.VideoNode(
+                source_id=self.url,
+                title=title,
+                files=[files.VideoFile(filepath)],
+                **kwargs
+            )
 
-        print("opening {}".format(self.file_on_disk))
 
-        # We'll add the title later when we iterate through the sections
-        topic_node = nodes.TopicNode(source_id=self.url, title='')
-        sections = soup.find_all('div', attrs={'class': 'section-heading'})
-        for section in sections:
-            # This is the top-level header, meaning it's the page title
-            title = section.text.strip()
-            if section.find('h1'):
-                print("Page title = {}".format(title))
-                topic_node.title = title
-                continue
+class ThumbnailTag(LinkTag):
+    default_ext = '.png'
+    directory = "img"
+    selector = ('div', {'class': 'sf-image'})
+    extra_scrapers = [FullscreenPageScraper]
+    scrape_subpages = False
 
-            print("Section = {}".format(title))
+    def format_url(self, url):
+        return get_absolute_url('https://www.who.int', url).replace('https://web-prod', 'https://www')
 
-            content = section.find_next_sibling()
-            if "content-block" in content.attrs['class']:
-                self.node_for_text_section(content)
-            elif "row" in content.attrs['class']:
-                # the section rows are siblings in the tree.
-                rows = [content]
-                next = content.find_next_sibling()
-                while "row" in next.attrs['class']:
-                    rows.append(next)
-                    next = next.find_next_sibling()
+    def get_link(self):
+        # Try to use the image if possible
+        img = self.tag.find('img')
+        if img:
+            link = img.get('src') or img.get('data-src') or ''
+        else:
+            link = self.tag.find('a') or self.tag.find_parent('a')
+            link = link and link['href']
+        return self.format_url(link)
 
-                self.node_for_rows(rows)
+    def process(self):
+        self.link = self.get_link()
+        if 'sf-image' in self.tag['class']:
+            div_tag = self.create_preview_tag(self.tag.find('img'))
+            if self.tag.parent.name == 'a':
+                self.tag.parent.replaceWith(div_tag)
+            else:
+                self.tag.replaceWith(div_tag)
 
-        return topic_node
+
+    def create_preview_tag(self, img):
+        # Some links might point to pdfs, so pass in img
+        div_tag = self.create_tag('div')
+        link_tag = self.get_scraper().to_tag()
+        img_url = img.get('data-src') or img.get('src')
+        if not img_url.startswith('img'):
+            img['src'] = self.write_url(self.format_url(img_url), directory='img')
+        img['style'] = []
+        self.mark_tag_to_skip(img)
+        link_tag.append(img)
+        div_tag.append(link_tag)
+        return div_tag
+
+
+class HighlightWidgetTag(ThumbnailTag):
+    default_ext = '.png'
+    directory = "img"
+    selector = ('div', {'class': 'highlight-widget'})
+    extra_scrapers = [FullscreenPageScraper]
+    scrape_subpages = False
+
+    def get_link(self):
+        link = self.tag.find('a') or self.tag.find_parent('a')
+        return link and link['href'].replace('https://web-prod', 'https://www')
+
+    def process(self):
+        if not self.tag.find('a'):
+            self.tag.decompose()
+            return
+
+        self.link = self.get_link()
+
+        # Don't scrape main title
+        if 'horizontal-title-only' in self.tag['class']:
+            self.tag.decompose()
+
+        # Links to other pages
+        elif 'image-on-top' in self.tag['class']:
+            self.create_external_link_tag()
+
+        # Infographics
+        elif 'title-only' in self.tag['class']:
+            img = self.tag.parent.find('img')
+            # Not all image links have available thumbnails, so make one
+            if not img:
+                img = self.create_tag('img')
+                img['src'] = self.link
+                preview_tag = self.create_preview_tag(img)
+                self.tag.replaceWith(preview_tag)
+            elif self.link.split('?')[0].endswith('.pdf'):
+                preview_tag = self.create_preview_tag(img)
+                self.tag.replaceWith(preview_tag)
+            else:
+                self.tag.decompose()
+
+    def create_external_link_tag(self):
+        # Create header
+        div = self.create_tag('div')
+        div['style'] = 'margin-bottom: 48px;text-align: center;'
+        h3 = self.create_tag('h3')
+        h3.string = self.tag.text.strip()
+        div.append(h3)
+
+        # Add image
+        if self.tag.find('div', {'class': 'background-image'}):
+            img = self.create_tag('img')
+            img_url = self.tag.find('div', {'class': 'background-image'})['data-image']
+            img['src'] = self.write_url(img_url, directory="img")
+            img['style'] = 'max-width: 450px; width: 100%;'
+            self.mark_tag_to_skip(img)
+            div.append(img)
+
+        # Add broken link copy
+        copy = self.create_copy_link_message(self.link)
+        div.append(copy)
+        self.tag.parent['class'] = []
+        self.tag.replaceWith(div)
+
+
+class FigureTag(HighlightWidgetTag):
+    selector = ('div', {'class': 'sf-multimedia-item__infographic'})
+
+    def process(self):
+        self.link = self.get_link()
+        img = self.create_tag('img')
+        img['src'] = self.link
+        preview_tag = self.create_preview_tag(img)
+        self.tag.replaceWith(preview_tag)
+
+
+class ExternalLinkTag(LinkTag):
+    def process(self):
+        if self.link:
+            self.tag.replaceWith(self.create_copy_link_message(self.link))
+
+
+class WHOPageScraper(HTMLPageScraper, ContentNodeMixin):
+    color = "#008DC9"
+    scrape_subpages = False
+    extra_tags = [FigureTag, ThumbnailTag, HighlightWidgetTag, ExternalLinkTag]
+    omit_list = [
+        ('script',),
+        ('header',),
+        ('ul', {'class': 'sf-breadscrumb'}),
+        ('footer', {'id': 'sf-footer'}),
+        ('input', {'type': 'hidden'}),
+    ]
+
+    @classmethod
+    def test(self, url):
+        return 'www.who.int' in url
+
+    def preprocess(self, contents):
+        # Remove side navigation
+        navigation = contents.find('div', {'class': 'left-navigation--wrapper'})
+        navigation.parent.decompose()
+
+        # Make sure styling is applied
+        main_row = contents.find('div', {'class': 'row'})
+        main_row['class'].append('sf-body')
+
+    def to_tag(self, *args):
+        return self.create_tag('div')
+
+class WHOVideoScraper(VideoScraper, ContentNodeMixin):
+    pass
+
+class WHOWebVideoScraper(WebVideoScraper, ContentNodeMixin):
+    pass
--- who-covid-advice/sushichef.py
+++ who-covid-advice/sushichef.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import json
 import os
+import re
+import sys
 
 from ricecooker.utils import downloader, html_writer
 from ricecooker.chefs import SushiChef
@@ -9,8 +11,14 @@ from ricecooker.config import LOGGER              # Use LOGGER to print messages
 from ricecooker.exceptions import raise_for_invalid_channel
 from le_utils.constants import exercises, content_kinds, file_formats, format_presets, languages
 
-from scrapers import who
+from bs4 import BeautifulSoup
+from hashlib import md5
+from io import BytesIO
+from pdf2image import convert_from_bytes
+from PIL import Image
 
+from webmixer.utils import guess_scraper
+from scrapers import who
 
 # Run constants
 ################################################################################
@@ -19,12 +27,61 @@ CHANNEL_SOURCE_ID = "who-covid-advice"                      # Unique ID for cont
 CHANNEL_DOMAIN = "who.int"                                  # Who is providing the content
 CHANNEL_LANGUAGE = "en"                                     # Language of channel
 CHANNEL_DESCRIPTION = None                                  # Description of the channel (optional)
-CHANNEL_THUMBNAIL = None                                    # Local path or url to image file (optional)
+CHANNEL_THUMBNAIL = "https://news.umanitoba.ca/wp-content/uploads/2020/04/WHO.jpg"             # Local path or url to image file (optional)
 
 # Additional constants
 ################################################################################
-
-
+BASE_URL = 'https://www.who.int/{language}/emergencies/diseases/novel-coronavirus-2019/advice-for-public/{endpoint}'
+SOURCE_MAP = {
+    'en': {
+        'description': 'Stay aware of the latest information on the COVID-19 outbreak, ' +
+                        'available on the WHO website and through your national and local ' +
+                        'public health authority. Most countries around the world have '+
+                        'seen cases of COVID-19 and many are experiencing outbreaks. ' +
+                        'Authorities in China and some other countries have succeeded in ' +
+                        'slowing their outbreaks. However, the situation is unpredictable ' +
+                        'so check regularly for the latest news.',
+    },
+    'ar': {
+        'description': "احرص على متابعة آخر المستجدات عن فاشية مرض كوفيد-19، على ال" +
+                       "موقع الإلكتروني لمنظمة الصحة العالمية ومن خلال سلطات الصحة ال" +
+                       "عامة المحلية والوطنية. وفي حين لا تزال عدوى كوفيد-19 متفشية ف" +
+                       "ي الصين بشكل أساسي، فهناك بعض بؤر التفشي في بلدان أخرى.  و" +
+                       "معظم الأفرادلذين يصابون بالعدوى يشعرون بأعراض خفيفة ويتعافون، و" +
+                       "لكن الأعراض قد تظهر بشكل أكثر حدة لدى غيرهم. احرص على العناية ب"  +
+                       "صحتك وحماية الآخرين بواسطة التدابير التالية:",
+    },
+    'es': {
+        'description': "Manténgase al día de la información más reciente sobre el brote de COVID-19," +
+                       " a la que puede acceder en el sitio web de la OMS y a través de las autoridades" +
+                       " de salud pública pertinentes a nivel nacional y local. La COVID-19 sigue afectando " +
+                       "principalmente a la población de China, aunque se han producido brotes en otros " +
+                       "países. La mayoría de las personas que se infectan padecen una enfermedad leve y se " +
+                       "recuperan, pero en otros casos puede ser más grave.",
+    },
+    'fr': {
+        'description': "Tenez-vous au courant des dernières informations sur la flambée de COVID-19, " +
+                       "disponibles sur le site Web de l’OMS et auprès des autorités de santé publique " +
+                       "nationales et locales. La COVID-19 continue de toucher surtout la population de " +
+                       "la Chine, même si des flambées sévissent dans d’autres pays. La plupart des " +
+                       "personnes infectées présentent des symptômes bénins et guérissent, mais d’autres " +
+                       "peuvent avoir une forme plus grave.",
+    },
+    'zh': {
+        'description': "请随时了解世卫组织网站上以及您所在国家和地方公共卫生机构提供的关于2019冠状病毒病疫情的最新信息。" +
+                       "世界上大多数国家都出现了COVID-19病例，许多国家正发生疫情。中国和其他一些国家已经成功地减缓了疫情。" +
+                       "但是，这种情况不可预测，因此请定期查看最新消息。",
+    },
+    'ru': {
+        'description': "Следите за новейшей информацией о вспышке COVID-19, которую можно найти на веб-сайте ВОЗ, " +
+                       "а также получить от органов общественного здравоохранения вашей страны и населенного пункта." +
+                       " Наибольшее число случаев COVID-19 по-прежнему выявлено в Китае, тогда как в других странах " +
+                       "отмечаются вспышки локального характера. В большинстве случаев заболевание характеризуется " +
+                       "легким течением и заканчивается выздоровлением, хотя встречаются осложнения.",
+    },
+}
+LICENSE = licenses.CC_BY_NC_SALicense(copyright_holder="World Health Organization")
+BLACKLIST = ['healthy-parenting']
 
 # The chef subclass
 ################################################################################
@@ -41,7 +98,6 @@ class WhoCovidAdviceChef(SushiChef):
         to build the contentnode tree.
     For more info, see https://ricecooker.readthedocs.io
     """
-    CONTENT_ARCHIVE_VERSION = 1
     DATA_DIR = os.path.abspath('chefdata')
     DOWNLOADS_DIR = os.path.join(DATA_DIR, 'downloads')
 
@@ -53,75 +109,21 @@ class WhoCovidAdviceChef(SushiChef):
         'CHANNEL_THUMBNAIL': CHANNEL_THUMBNAIL,
         'CHANNEL_DESCRIPTION': CHANNEL_DESCRIPTION,
     }
+    language = 'en'  # Default to English if no language is provided
+    options = None
 
-    # Since this is a multi-lingual site but pages can't be scraped in a consistent fashion, we use page IDs
-    # so that we can logically refer to specific pages in the scraping code without relying on language specific
-    # names, etc.
-    page_data = {
-        'covid_advice': {
-            'url': 'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public',
-        },
-        'face_masks': {
-            'url': 'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public/when-and-how-to-use-masks'
-        },
-        'mythbusters': {
-            'url': 'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public/myth-busters'
-        },
-        'videos': {
-            'url': 'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public/videos'
-        },
-        'advocacy': {
-            'url': 'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public/healthy-parenting'
-        }
-    }
+    def __init__(self, *args, **kwargs):
+        super(WhoCovidAdviceChef, self).__init__(*args, **kwargs)
+        self.options = kwargs
 
-    tree_structure = [
-        {
-            'covid_advice': [
-                {
-                    'face_masks': []
-                },
-                {
-                    'mythbusters': []
-                },
-                {
-                    'videos': []
-                },
-                {
-                    'advocacy': []
-                }
-            ]
-        }
-    ]
-
-    def download_content(self):
-        self.CONTENT_DIR = os.path.join(self.DOWNLOADS_DIR, 'archive_{}'.format(self.CONTENT_ARCHIVE_VERSION))
-
-        archive_data_filename = os.path.join(self.CONTENT_DIR, 'archive_data.json')
-        if os.path.exists(archive_data_filename):
-            archive_data = json.load(open(archive_data_filename))
-            self.page_data = archive_data
-
-        for page_id in self.page_data:
-            url = self.page_data[page_id]['url']
-            # don't re-download if we've already got a successful download.
-            if not 'download_info' in self.page_data[page_id]:
-                self.page_data[page_id]['download_info'] = downloader.archive_page(url, self.CONTENT_DIR)
-
-        with open(archive_data_filename, 'w') as f:
-            archive_data = json.dumps(self.page_data, indent=4)
-            f.write(archive_data)
-
-    def page_to_topic(self, page_id):
-        """
-        Because each page uses a different structure and format, we can't reuse a lot of the scraping logic.
-        This function checks page_id and redirects to the appropriate scraping code for that page.
-        :param page_id:
-        :return:
-        """
-
-        scraper = who.WHOCovidAdvicePageScraper(self.page_data[page_id]['url'], self.page_data[page_id]['download_info']['index_path'])
-        return scraper.get_ricecooker_node()
+    def get_channel(self, *args, **kwargs):
+        channel = super(WhoCovidAdviceChef, self).get_channel(*args, **kwargs)
+        self.language = self.options['language']
+        channel.language = self.language
+        channel.source_id = '{}-{}'.format(channel.source_id, self.language)
+        channel.description = SOURCE_MAP[self.language]['description']
+        channel.source_domain = BASE_URL.format(language=self.language, endpoint='')
+        return channel
 
     def construct_channel(self, *args, **kwargs):
         """
@@ -134,19 +136,86 @@ class WhoCovidAdviceChef(SushiChef):
         Returns: ChannelNode
         """
         channel = self.get_channel(*args, **kwargs)  # Create ChannelNode from data in self.channel_info
+        LOGGER.info('Scraping {}'.format(self.language))
 
-        # FIXME: this should use the tree_structure instead, this is just for testing the conversion.
-        for page_id in self.page_data:
-            print("Converting page {} to topic".format(page_id))
-            self.page_to_topic(page_id)
+        # Scrape advice page to html zips
+        LOGGER.info('  Scraping advice page')
+        main_url = BASE_URL.format(language=self.language, endpoint='')
+        contents = BeautifulSoup(downloader.read(main_url), 'html5lib')
+        title = contents.find('div', {'class': 'section-heading'}).text
+        html = self.scrape_page_to_html(main_url, title)
+        channel.add_child(html)
+
+        # Get available topics
+        for topic in contents.find('ul', {'class': 'accordion-content'}).findAll('a'):
+            LOGGER.info('    {}'.format(topic.text.strip().encode('utf-8-sig')))
+            endpoint = topic['href'].split('/')[-1]
+            topic_url = BASE_URL.format(language=self.language, endpoint=endpoint)
+            if endpoint == 'videos':
+                video = self.scrape_video_page(topic_url, topic.text.strip())
+                channel.add_child(video)
+            elif endpoint not in BLACKLIST:
+                html = self.scrape_page_to_html(topic_url, topic.text.strip())
+                channel.add_child(html)
 
         return channel
 
+    def scrape_page_to_html(self, url, title):
+        """ Creates an html node based on the url """
+        scraper = who.WHOPageScraper(url, locale=self.language)
+        return scraper.to_contentnode(title, directory=self.DOWNLOADS_DIR, license=LICENSE)
+
+    def scrape_video_page(self, url, title):
+        """ Creates a video topic with all the videos on the page """
+        IGNORED_VIDEOS = ['google', 'facebook']
+        VIDEO_SCRAPERS = [who.WHOWebVideoScraper, who.WHOVideoScraper]
+
+        video_topic = nodes.TopicNode(source_id=url, title=title)
+        contents = BeautifulSoup(downloader.read(url), 'html.parser')
+
+        # Scrape youtube embeds
+        # e.g. https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public/videos
+        for iframe in contents.findAll('iframe'):
+            if not any([test for test in IGNORED_VIDEOS if test in iframe['src']]):
+                header = iframe.find_parent('div', {'class': 'sf_colsIn'}).find('div', {'class': 'section-heading'}).text.strip()
+                LOGGER.info('      - Downloading {}'.format(header.encode('utf-8')))
+                scraper = guess_scraper(iframe['src'], scrapers=VIDEO_SCRAPERS) # Might be native or youtube video
+                video_node = scraper.to_contentnode(header, license=LICENSE, directory="videos")
+                video_topic.add_child(video_node)
+
+        # Scrape native videos
+        # e.g. https://www.who.int/zh/emergencies/diseases/novel-coronavirus-2019/advice-for-public/videos
+        for video in contents.findAll('div', {'class': 'sf-multimedia-item__video'}):
+            header = video.find('h3').text.strip()
+            LOGGER.info('      - Downloading {}'.format(header.encode('utf-8')))
+            video_matches = re.search(r"\(\s*\"(.+)\"\,\s*\"(.+)\"\)", video.find('a')['onclick'])
+
+            # Embedded youtube videos here refer to playlists, so skip them
+            if 'YoutubeVideo' == video_matches.group(1):
+                continue
+
+            scraper = who.WHOVideoScraper(video_matches.group(2))
+            video_node = scraper.to_contentnode(header, license=LICENSE, directory="videos")
+            video_topic.add_child(video_node)
+
+        return video_topic
+
+
+""" Utility function to check if any new languages have been added """
+def get_available_languages():
+    contents = BeautifulSoup(downloader.read(BASE_URL.format(language='en', endpoint='')), 'html5lib')
+    languages = []
+    for lang in contents.find('ul', {'class': 'sf-lang-selector'}).findAll('li'):
+        languages.append(re.search(r"openLinkWithTranslation\('([^\']+)'\)", lang.find('a')['onclick']).group(1))
+    return languages
 
 
 # CLI
 ################################################################################
 if __name__ == '__main__':
-    # This code runs when sushichef.py is called from the command line
-    chef = WhoCovidAdviceChef()
-    chef.main()
+    for language in get_available_languages():
+        if not SOURCE_MAP.get(language):
+            LOGGER.warning('{} is not listed in the SOURCE_MAP'.format(language))
+            continue
+        chef = WhoCovidAdviceChef(language=language)
+        chef.main()
