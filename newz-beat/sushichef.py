473cda7884b47e3e7505dd892440bed4085a43de
kollivier <kevino@theolliviers.com>
2020-10-23 16:49:36 -0700

    Initial chef commit.

---
+++ newz-beat/sushichef.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import os
+import sys
+from ricecooker.utils import downloader, html_writer
+from ricecooker.chefs import YouTubeSushiChef
+from ricecooker.classes import nodes, files, questions, licenses
+from ricecooker.config import LOGGER              # Use LOGGER to print messages
+from ricecooker.exceptions import raise_for_invalid_channel
+from le_utils.constants import exercises, content_kinds, file_formats, format_presets, languages
+
+
+# Run constants
+################################################################################
+CHANNEL_ID = "32e5033ebc7a456b91fccbd2747d4035"             # UUID of channel
+CHANNEL_NAME = "Newz Beat"                           # Name of Kolibri channel
+CHANNEL_SOURCE_ID = "<yourid>"                              # Unique ID for content source
+CHANNEL_DOMAIN = "<yourdomain.org>"                         # Who is providing the content
+CHANNEL_LANGUAGE = "en"                                     # Language of channel
+CHANNEL_DESCRIPTION = None                                  # Description of the channel (optional)
+CHANNEL_THUMBNAIL = None                                    # Local path or url to image file (optional)
+CONTENT_ARCHIVE_VERSION = 1                                 # Increment this whenever you update downloaded content
+
+
+# Additional constants
+################################################################################
+
+
+
+# The chef subclass
+################################################################################
+class NewzBeatChef(YouTubeSushiChef):
+    """
+    This class converts content from the content source into the format required by Kolibri,
+    then uploads the {channel_name} channel to Kolibri Studio.
+    Your command line script should call the `main` method as the entry point,
+    which performs the following steps:
+      - Parse command line arguments and options (run `./sushichef.py -h` for details)
+      - Call the `SushiChef.run` method which in turn calls `pre_run` (optional)
+        and then the ricecooker function `uploadchannel` which in turn calls this
+        class' `get_channel` method to get channel info, then `construct_channel`
+        to build the contentnode tree.
+    For more info, see https://ricecooker.readthedocs.io
+    """
+    channel_info = {
+        'CHANNEL_ID': CHANNEL_ID,
+        'CHANNEL_SOURCE_DOMAIN': CHANNEL_DOMAIN,
+        'CHANNEL_SOURCE_ID': CHANNEL_SOURCE_ID,
+        'CHANNEL_TITLE': CHANNEL_NAME,
+        'CHANNEL_LANGUAGE': CHANNEL_LANGUAGE,
+        'CHANNEL_THUMBNAIL': CHANNEL_THUMBNAIL,
+        'CHANNEL_DESCRIPTION': CHANNEL_DESCRIPTION,
+    }
+
+
+
+# CLI
+################################################################################
+if __name__ == '__main__':
+    # This code runs when sushichef.py is called from the command line
+    chef = NewzBeatChef()
+    chef.main()
