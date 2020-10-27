888e1d41de845f90f117425ea5009a4a67573f50
richard-dinh <richarddinh@gmail.com>
2020-10-27 12:20:50 -0700

    updated constants and added logo

--- chicken-and-chips/sushichef.py
+++ chicken-and-chips/sushichef.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 import os
 import sys
-from ricecooker.utils import downloader, html_writer
 from ricecooker.chefs import YouTubeSushiChef
-from ricecooker.classes import nodes, files, questions, licenses
-from ricecooker.config import LOGGER              # Use LOGGER to print messages
-from ricecooker.exceptions import raise_for_invalid_channel
-from le_utils.constants import exercises, content_kinds, file_formats, format_presets, languages
+from ricecooker.classes import licenses
+
+from googleapiclient.discovery import build
 
 
 # Run constants
 ################################################################################
 CHANNEL_ID = "04deda9bf5b5489689191d4f2bcbaf74"             # UUID of channel
 CHANNEL_NAME = "Chicken N Chips"                           # Name of Kolibri channel
-CHANNEL_SOURCE_ID = "<yourid>"                              # Unique ID for content source
-CHANNEL_DOMAIN = "<yourdomain.org>"                         # Who is providing the content
+CHANNEL_SOURCE_ID = "chicken-n-chips"                              # Unique ID for content source
+CHANNEL_DOMAIN = "youtube.com"                         # Who is providing the content
 CHANNEL_LANGUAGE = "en"                                     # Language of channel
-CHANNEL_DESCRIPTION = None                                  # Description of the channel (optional)
-CHANNEL_THUMBNAIL = None                                    # Local path or url to image file (optional)
+CHANNEL_DESCRIPTION = 'Chicken & Chips is a weekly pop culture magazine, that focuses on relationships and combines music, journalism, puppetry and animation to create the freshest blend of edutainment in Uganda. Airs every Sunday at 1:30pm on NTV Uganda'                                  # Description of the channel (optional)
+CHANNEL_THUMBNAIL = os.path.join('files', 'logo.png')       # Local path or url to image file (optional)
 CONTENT_ARCHIVE_VERSION = 1                                 # Increment this whenever you update downloaded content
 
 
 # Additional constants
 ################################################################################
-
+# Add GOOGLE API key here. Will need access to Youtube API v3
+GOOGLE_API_KEY = None
+CHICKEN_N_CHIPS_CHANNEL_ID = 'UCIGQCJIF4fdTrqxnvcwTYxg'
 
 
 # The chef subclass
