22a9ff573aedfae32c0b63151bb511d0466329e3
richard-dinh <richarddinh@gmail.com>
2020-11-16 11:49:01 -0800

    added utils and logo

---
+++ sesamath/utils.py
@@ -0,0 +1,15 @@
+import os
+
+SESAMATH_URL_FORMAT = "https://mathenpoche.sesamath.net/?page={0}"
+
+SESAMATH_THUMBNAIL_PATH = os.path.join("files", "sesamath_logo.png")
+
+GRADE_MAP = [
+  "https://mathenpoche.sesamath.net/?page=sixieme",
+  "https://mathenpoche.sesamath.net/?page=cinquieme",
+  "https://mathenpoche.sesamath.net/?page=quatrieme",
+  "https://mathenpoche.sesamath.net/?page=troisieme",
+  "https://mathenpoche.sesamath.net/?page=seconde",
+  "https://mathenpoche.sesamath.net/?page=premiere",
+  "https://mathenpoche.sesamath.net/?page=terminale"
+]
