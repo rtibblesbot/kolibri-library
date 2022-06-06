b93807021c1630f6aceef229ad66261b03d7fbb0
katanasovski <katanasovski@melontech.com>
2022-06-06 11:59:56 +0200

    cleanup on unused things

--- african-storybook/chef.py
+++ african-storybook/chef.py
@@ -85,7 +85,7 @@ class AfricanStorybookChef(SushiChef):
     """
     channel_info = {
         'CHANNEL_SOURCE_DOMAIN': "www.africanstorybook.org",
-        'CHANNEL_SOURCE_ID': "african-storybook_2",
+        'CHANNEL_SOURCE_ID': "african-storybook",
         'CHANNEL_TITLE': "African Storybook Library (multiple languages)",
         'CHANNEL_THUMBNAIL': "thumbnail.png",
         'CHANNEL_DESCRIPTION': "Library of picture storybooks in all the languages of African countries, designed to promote basic literacy and reading for learners of young ages and varying literacy levels.",
