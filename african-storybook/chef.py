9087169a42362bbebdf22709ab12608833d94d5b
katanasovski <katanasovski@melontech.com>
2023-11-13 13:13:38 +0100

    Optimization and documentation

--- african-storybook/chef.py
+++ african-storybook/chef.py
@@ -23,8 +23,6 @@ from ricecooker.classes import nodes, files, licenses
 from ricecooker.config import LOGGER
 from ricecooker.utils.caching import CacheForeverHeuristic, FileCache, CacheControlAdapter
 from ricecooker.utils.html import download_file, WebDriver
-from ricecooker.utils.zip import create_predictable_zip
-
 NETWORK_ERRORS = (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout)
 
 CHEF_TMPDIR = 'chefdata/tmp/'  # use local tmp dir for saving HTML5 Apps content
@@ -47,21 +45,16 @@ headers = {
 }
 
 FOLDER_STORAGE = os.path.join(os.getcwd(), 'chefdata', "storage")
-FOLDER_STORAGE_PARALLEL = os.path.join(os.getcwd(), 'chefdata', "parallel")
-FOLDER_STORAGE_BROWSER = os.path.join(os.getcwd(), 'chefdata', "test3")
 # copyright_holder = 'African Storybook Initiative'
 COPYRIGHT_HOLDER = 'African Storybook Initiative'
 
 if not os.path.exists(FOLDER_STORAGE):
     os.mkdir(FOLDER_STORAGE)
 
-if not os.path.exists(FOLDER_STORAGE_PARALLEL):
-    os.mkdir(FOLDER_STORAGE_PARALLEL)
+if not os.path.exists(FOLDER_STORAGE):
+    os.mkdir(FOLDER_STORAGE)
 
-if not os.path.exists(FOLDER_STORAGE_BROWSER):
-    os.mkdir(FOLDER_STORAGE_BROWSER)
-
-ABS_FOLDER_STORAGE_BROWSER = os.path.abspath(FOLDER_STORAGE_BROWSER)
+ABS_FOLDER_STORAGE_BROWSER = os.path.abspath(FOLDER_STORAGE)
 
 
 def get_lang_by_name_with_fallback(language_name):
@@ -82,7 +75,7 @@ class AfricanStorybookChef(SushiChef):
     """
     channel_info = {
         'CHANNEL_SOURCE_DOMAIN': "www.africanstorybook.org",
-        'CHANNEL_SOURCE_ID': "african-storybook",
+        'CHANNEL_SOURCE_ID': "african-storybook-test",
         'CHANNEL_TITLE': "African Storybook Library (multiple languages)",
         'CHANNEL_THUMBNAIL': "asb120.png",
         'CHANNEL_DESCRIPTION': "Library of picture storybooks in all the languages of African countries, designed to promote basic literacy and reading for learners of young ages and varying literacy levels.",
@@ -94,6 +87,8 @@ class AfricanStorybookChef(SushiChef):
         """
         # create channel
         channel_info = self.channel_info
+        print(channel_info.get('CHANNEL_THUMBNAIL'))
+
         channel = nodes.ChannelNode(
             source_domain=channel_info['CHANNEL_SOURCE_DOMAIN'],
             source_id=channel_info['CHANNEL_SOURCE_ID'],
@@ -103,11 +98,10 @@ class AfricanStorybookChef(SushiChef):
             language="mul",
         )
 
-        books, dict_languages = get_languages_and_books()
+        dict_books, dict_languages = get_languages_and_books()
         LOGGER.debug('STARTING TO CREATE THE RICECOOKER CHANNEL TREE')
         dict_node_languages = {}
         dict_node_levels = OrderedDict()
-
         for key_language in dict_languages:
             if key_language == "0":
                 LOGGER.info('skipping language 0')
@@ -119,11 +113,12 @@ class AfricanStorybookChef(SushiChef):
             language_node = nodes.TopicNode(source_id=language, title=language, language=lang_obj.code)
             dict_node_languages[key_language] = language_node
             channel.add_child(language_node)
-
-        for book in books:
+        for key in dict_books:
+            book = dict_books[key]
             book_lang = dict_languages.get(book.get('lang'))
             if book_lang:
                 lang_node = dict_node_languages.get(book.get('lang'))
+                print(lang_node)
                 if not dict_node_levels.get(book_lang) or not dict_node_levels.get(book_lang).get(book.get('level')):
                     topic_level_node = nodes.TopicNode(source_id="{}-{}".format(book_lang, book.get('level')),
                                                        title='Level {}'.format(book.get('level')),
@@ -147,56 +142,72 @@ class AfricanStorybookChef(SushiChef):
                     author=truncate_metadata(book.get('author')),
                     files=[files.EPubFile("{}".format(book_path))],
                     language=get_lang_by_name_with_fallback(book_lang),
+                    derive_thumbnail=True
                 )
                 topic_level_node.add_child(book_node)
         return channel
 
-
+#346310
 async def download_all_epubs():
-    dict_page_download = {}
-    browser = await launch(headless=True)
-    # await browser.close()
+    """
+    Automatic open browser, load the page. Wait ( sleep ) to load whole content.
+    Click the menu and wait to open the new content
+    Take all the books from console variable.
+    List all the books and download one by one.
+    """
+    browser = await launch(headless=False)
     pages = await browser.pages()
     page = pages[0]
     await page.goto('https://www.africanstorybook.org/', {'waitUntil': 'networkidle2'})
     await page._client.send('Page.setDownloadBehavior',
-                            {'behavior': 'allow', 'downloadPath': FOLDER_STORAGE_BROWSER, 'waitUntil': 'networkidle2'})
+                            {'behavior': 'allow', 'downloadPath': FOLDER_STORAGE, 'waitUntil': 'networkidle2'})
+    time.sleep(5)
+    cookies = await page.waitForSelector('#c-p-bn')
+    await cookies.click()
     time.sleep(10)
+    element_menu = await page.JJ('#menuControl > a')
+    time.sleep(1)
+    await element_menu[0].click()
+    time.sleep(1)
+    list_menu = await page.JJ('.close-popover')
+    time.sleep(1)
+    await list_menu[0].click()
+    time.sleep(2)
     books = await page.evaluate('bookItems')
-    lst_books = os.listdir(FOLDER_STORAGE_BROWSER)
+    lst_books = os.listdir(FOLDER_STORAGE)
     await page.close()
-    # books = books[0:1000]
     for book in books:
         book_name = "asb{}.epub".format(book.get('id'))
         if book_name not in lst_books:
             LOGGER.info("Book name %s" % book_name)
-            page, browser = await download_epub_book(book.get('id'))
-            dict_page_download[book_name] = {'page': page, 'browser': browser}
-            lst_finished_dl_book = await find_finished_download(dict_page_download)
-            if len(lst_finished_dl_book) > 1:
-                for book_name_finish in lst_finished_dl_book:
-                    dict_page_browser = dict_page_download.get(book_name_finish)
-                    if not dict_page_browser.get('page').isClosed():
-                        page_finished = dict_page_browser.get('page')
-                        browser_finished = dict_page_browser.get('browser')
-                        await page_finished.close()
-                        await browser_finished.close()
-                        dict_page_download.pop(book_name_finish)
+            file_path = download_epub_book(book.get('id'))
+            print("Downloaded ended %s" % file_path)
         else:
             LOGGER.info("Book name %s already exists" % book_name)
     await browser.close()
 
 
-async def download_epub_book(book_id):
-    browser = await launch(headless=True)
-    pages = await browser.pages()
-    page = pages[0]
-    await page.goto('https://www.africanstorybook.org/', {'waitUntil': 'networkidle2'})
-    await page._client.send('Page.setDownloadBehavior',
-                            {'behavior': 'allow', 'downloadPath': FOLDER_STORAGE_BROWSER, 'waitUntil': 'networkidle2'})
-    await page.evaluate(pageFunction="doDownloadEpub('{}')".format(book_id))
-    LOGGER.info("Downloading book %s" % (book_id))
-    return page, browser
+def download_epub_book(book_id):
+    """
+    When the book is load donwload it.
+    """
+    try:
+        # browser = await launch(headless=False)
+        # pages = await browser.pages()
+        # page = pages[0]
+        download_url = 'https://www.africanstorybook.org/makeapp/data/landscape.php?id={}'.format(book_id)
+        # await page.goto('https://www.africanstorybook.org/', {'waitUntil': 'networkidle2'})
+        # await page._client.send('Page.setDownloadBehavior',
+        #                         {'behavior': 'allow', 'downloadPath': FOLDER_STORAGE, 'waitUntil': 'networkidle2'})
+        # await page.evaluate(pageFunction="doDownloadEpub('{}')".format(book_id))
+        respones = requests.get(download_url)
+        file_path = os.path.join(FOLDER_STORAGE, 'asb{}.epub'.format(book_id))
+        with open(file_path, 'wb') as f:
+            f.write(respones.content)
+        LOGGER.info("Downloading book %s" % (book_id))
+        return file_path
+    except Exception as ex:
+        print(ex)
 
 
 async def find_finished_download(dict_page_download):
@@ -209,14 +220,21 @@ async def find_finished_download(dict_page_download):
 
 def get_languages_and_books():
     with WebDriver("https://www.africanstorybook.org/", delay=10000) as driver:
-        books = driver.execute_script("return bookItems;")
+        time.sleep(5)
+        books = driver.execute("return bookItemsAppr;")
+        dict_books = {}
+        for book in books:
+            print(book)
+            if book.get('id') not in dict_books:
+                dict_books = book
+
         languages_html = driver.execute_script("return languages;")
         language_id_map = {}
         bs_html_page = BeautifulSoup(languages_html, "html.parser")
         for node in bs_html_page:
             language_id_map[node["value"]] = node.text.strip()
 
-    return books, language_id_map
+        return dict_books, language_id_map
 
 
 def strip_level_from_title(title):
